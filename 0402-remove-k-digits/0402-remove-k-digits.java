b7ec3da370879c6cabe9efcb46e6dfd557152ac8
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-06-20 11:36:45 -0400

    Time: 88 ms (12.26%), Space: 45.8 MB (35.38%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -2,38 +2,39 @@ class Solution {
     public String removeKdigits(String num, int k) {
         char[] c = num.toCharArray();
 
-        int allowed = num.length()-k;
+        int allowed = num.length() - k;
 
         Stack<Character> s = new Stack<>();
-        for(int i =0; i < c.length ; i++){
-            while(!s.isEmpty() && s.peek() -'0' > c[i] -'0' ){
+        for (int i = 0; i < c.length; i++) {
+            while (!s.isEmpty() && s.peek() - '0' > c[i] - '0') {
 
-                if(k == 0){
+                if (k == 0) {
                     break;
-                }
-                else{
+                } else {
                     s.pop();
                     k--;
                 }
             }
 
-            if(s.size() < allowed){
+            if (s.size() < allowed) {
 
-            s.push(c[i]);
-            }
-            else{
+              
+
+                    s.push(c[i]);
+                
+                }
+            else {
                 k--;
             }
         }
 
+        String ans = "";
 
-        String ans ="";
-
-        while(!s.isEmpty()){
-            ans=String.valueOf(s.pop()).concat(ans);
+        while (!s.isEmpty()) {
+            ans = String.valueOf(s.pop()).concat(ans);
         }
 
-        if(ans.isEmpty()){
+        if (ans.isEmpty()) {
             return "0";
         }
 
@@ -43,7 +44,5 @@ class Solution {
         }
         return ans.substring(idx);
 
-
-        
     }
 }
