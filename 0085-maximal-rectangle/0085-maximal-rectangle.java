5e0349fbd6dc0399f78383fe27cf019a1417ca83
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-06-21 10:56:03 -0400

    Time: 20 ms (36.02%), Space: 46.8 MB (41.4%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,79 +1,74 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
 
-      
-        
-
         int[] temp = new int[matrix[0].length];
 
-        int maxArea =0;
+        int maxArea = 0;
 
-        for (int i = 0; i < matrix.length ; i++){
+        for (int i = 0; i < matrix.length; i++) {
             int[] temp2 = new int[matrix[0].length];
 
-            for(int j =0; j < matrix[0].length ;j++){
-                if(matrix[i][j] == '0'){
-                     temp2[j] = 0;
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == '0') {
+                    temp2[j] = 0;
+                } else {
+                    temp2[j] = (matrix[i][j] - '0') + temp[j];
                 }
-                else{
-                    temp2[j] = (matrix[i][j] -'0') + temp[j];
+            }
+
+          
+
+            Stack<Integer> s = new Stack<>();
+
+            
+
+            for (int k = 0; k < temp2.length; k++) {
+                //calculate pse
+
+                int pse = 0;
+
+                while (!s.isEmpty() && temp2[s.peek()] >= temp2[k]) {
+                    int el = s.pop();
+
+                    int nse = k;
+                    if (s.isEmpty()) {
+                        pse = -1;
+                    } else {
+                        pse = s.peek();
+                    }
+
+                    int area = (nse - pse - 1) * temp2[el];
+
+                    maxArea = Math.max(area, maxArea);
                 }
-            }
-        
 
+                s.push(k);
 
-
-        int[] nse = new int[matrix[0].length];
-
-
-        Stack<Integer> s = new Stack<>();
-
-        for(int k = temp2.length-1; k >=0 ;k--){
-            while(!s.isEmpty() && temp2[s.peek()] >= temp2[k]  ){
-                s.pop();
             }
 
-            if(s.isEmpty()){
-                nse[k] =temp2.length;
-            }
-            else{
-                nse[k] = s.peek();
-            }
-            s.push(k);
-        }
-
-
-
-        s.clear();
-
-        for(int k=0 ; k < temp2.length ;k++){
-            //calculate pse
-
+            int nse = temp2.length;
             int pse = 0;
 
-            while(!s.isEmpty() && temp2[s.peek()] >= temp2[k]){
-                s.pop();
+            while (!s.isEmpty()) {
+
+                int el = s.pop();
+
+               
+                if (s.isEmpty()) {
+                    pse = -1;
+                } else {
+                    pse = s.peek();
+                }
+
+                int area = (nse - pse - 1) * temp2[el];
+
+                maxArea = Math.max(area, maxArea);
+
             }
 
-            if(s.isEmpty()){
-                pse = -1;
-            }
-            else{
-
-                pse = s.peek();
-            }
-            s.push(k);
-
-
-            int area = (nse[k] - pse-1)*temp2[k];
-
-            maxArea =Math.max(area,maxArea);
-
-
+            temp = temp2;
         }
 
-        temp =temp2;
-    } 
-
-    return maxArea;
-}}
+        return maxArea;
+    }
+}
