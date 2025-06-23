7f615bad3c994d100f6dcd08cf69e2b64e43e311
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-06-23 09:26:19 -0400

    Time: 32 ms (36.23%), Space: 48.5 MB (97.6%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.java
+++ 0937-online-stock-span/0937-online-stock-span.java
@@ -1,8 +1,8 @@
-class Pair<U, V> {
+class Pair<U,V>  {
     public U first;
     public V second;
 
-    public Pair(U first, V second) {
+    public Pair(U first, V  second) {
         this.first = first;
         this.second = second;
     }
