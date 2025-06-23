31bef4dbe00804296ccea27854d92e3de9fe9cf6
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-06-23 09:09:02 -0400

    Time: 37 ms (11.69%), Space: 56.2 MB (6.57%) - LeetHub

---
+++ 0937-online-stock-span/0937-online-stock-span.java
@@ -0,0 +1,47 @@
+class StockSpanner {
+    int idx =0;
+
+    Stack<Integer> s ;
+    Map<Integer , Integer> map  ;
+
+    public StockSpanner() {
+
+        
+        
+        s =new Stack<Integer>();
+        map = new HashMap<>();
+    }
+    
+    public int next(int price) {
+        int span = 1;
+        
+        
+            while(!s.isEmpty() && map.get(s.peek()) <= price){
+                   s.pop();
+            }
+
+            if(s.isEmpty()){
+                 span = idx -(-1);
+            }
+            else{
+                span = idx - s.peek();
+            }
+
+           
+        
+        map.put(idx , price);
+        s.push(idx);
+        idx++;
+
+        return span;
+
+
+
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
