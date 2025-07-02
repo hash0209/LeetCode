d6e9cb98367cfe5ca6a01f2058f8209ba388b4e2
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-07-02 09:26:18 -0400

    Time: 2 ms (83.22%), Space: 45.6 MB (70.92%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,39 +1,19 @@
 class Solution {
     public boolean canJump(int[] nums) {
-       
-        boolean[] arr = new boolean[nums.length];
-        arr[nums.length-1] = true;
+       int nearidx =nums.length-1;
+      for(int i = nums.length-2; i >= 1 ;i--){
+           int maxJumps = nums[i];
+           if(i+maxJumps >= nearidx){
+            nearidx = i;
+           }
+       }
 
+       if(nums[0] >= nearidx){
+        return true;
+       }
+       else{
+        return false;
+       }
 
-        int indexToReach = nums.length-1;
-
-
-        for(int i =  nums.length-2 ; i >= 0; i--){
-              int maxJumps = nums[i];
-
-              int jump=1;
-              boolean canReach = false;
-
-              while(jump <= maxJumps){
-
-                   if(i+jump > indexToReach){
-                    break;
-                   }
-                   if(i+jump == indexToReach){
-                    canReach=true;
-                    break;
-                   }
-                    if(arr[i+jump] == true){
-                    canReach=true;
-                    break;
-                   }
-                   jump++;
-              }
-
-
-              arr[i] = canReach;
-        }
-
-        return arr[0];
     }
 }
