c0f298fb42b22ceca8d695dce33fcc1356c4f133
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-07-10 10:56:54 -0400

    Time: 1 ms (98.54%), Space: 42.4 MB (85.21%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -16,25 +16,36 @@
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> ans = new ArrayList<>();
-        dfs(root,"",ans);
+        StringBuilder s = new StringBuilder();
+        dfs(root,s,ans);
         return ans;
     }
 
-    public void dfs(TreeNode root, String path, List<String> ans) {
+    public void dfs(TreeNode root, StringBuilder path, List<String> ans) {
 
 
         if(root == null){
             return;
         }
 
-        path += (path.length() > 0 ? "->" : "") + root.val;
+       
+
+      //  path += (path.length() > 0 ? "->" : "") + root.val;
+        if(path.length() > 0){
+            path.append("->");
+        }
+
+        path.append(root.val);
+
+        int len = path.length();
 
         if(root.left == null && root.right ==null){
-            ans.add(path);
+            ans.add(path.toString());
             return ;
         }
 
         dfs(root.left,path,ans);
+        path.setLength(len);
         dfs(root.right,path,ans);
 
 
