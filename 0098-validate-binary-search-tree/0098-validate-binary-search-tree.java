3bc67095511c505cdfde01998c1b7aee7ba589c7
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-07-17 11:38:16 -0400

    Time: 0 ms (100%), Space: 44.5 MB (20.38%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,52 +15,21 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
+          return  find(root , Long.MIN_VALUE , Long.MAX_VALUE);
+     
+    }
 
-       if(root == null){
-        return true ; 
-       }
+    public boolean find(TreeNode root , Long min , Long max){
+          if(root == null){
+            return true;
+          }
 
-        if(root.left != null){
-           
-           if(root.val <= root.left.val ){
-            return false;
-           }
+          if(root.val > min && root.val < max){
 
-            TreeNode temp = root.left.right;
+            
+            return find(root.left , min , (long)root.val) && find(root.right , (long)root.val , max);
+          }
 
-
-            while(temp!=null && temp.right!=null){
-                temp = temp.right;
-            }
-
-            if(temp!=null && temp.val >= root.val){
-                return false;
-            }
-
-        }
-
-
-        if(root.right != null){
-           
-           if(root.val >= root.right.val ){
-            return false;
-           }
-
-            TreeNode temp = root.right.left;
-
-
-            while(temp!=null && temp.left!=null){
-                temp = temp.left;
-            }
-
-            if(temp != null && temp.val <= root.val){
-                return false;
-            }
-
-        }
-
-
-        return isValidBST(root.left) && isValidBST(root.right);
-        
+          return false;
     }
 }
