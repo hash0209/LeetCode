0615f20f94d532ad7872b28aeca537277794986e
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-07-20 10:43:40 -0400

    Time: 0 ms (100%), Space: 40.8 MB (96.49%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,74 +1,63 @@
 class Solution {
     public int rob(int[] nums) {
 
-        if(nums.length == 1){
+        if (nums.length == 1) {
             return nums[0];
         }
 
-        int [][] dp1 = new int[nums.length][2];
-   //last picked 
+        int[][] dp = new int[nums.length][2];
+        //last picked 
 
-        dp1[0][0] =  0;
-        dp1[0][1] = 0;
+        dp[0][0] = 0;
+        dp[0][1] = 0;
 
-        for(int idx =1; idx < nums.length-1 ; idx ++){
-                for(int p =0 ; p <=1 ; p++){
+        for (int idx = 1; idx < nums.length - 1; idx++) {
+            for (int p = 0; p <= 1; p++) {
 
-                    
-                    int pick = Integer.MIN_VALUE; 
+                int pick = Integer.MIN_VALUE;
 
-                    if(p == 1){
-                       pick = dp1[idx-1][0] + nums[idx];
+                if (p == 1) {
+                    pick = dp[idx - 1][0] + nums[idx];
 
-                       
-                    }
-
-                  //  int notPick = Integer.MIN_VALUE;
-
-                   
-                    int notPick = dp1[idx-1][1];
-
-                    dp1[idx][p] = Math.max(pick,notPick);
                 }
+
+                //  int notPick = Integer.MIN_VALUE;
+
+                int notPick = dp[idx - 1][1];
+
+                dp[idx][p] = Math.max(pick, notPick);
+            }
         }
 
-        int ans11 = dp1[nums.length-2][0] + nums[nums.length-1];
-
-        
+        int ans11 = dp[nums.length - 2][0] + nums[nums.length - 1];
 
         //not picked
 
-        int [][] dp2 = new int[nums.length][2];
+        // int [][] dp2 = new int[nums.length][2];
 
-        dp2[0][0] =  0;
-        dp2[0][1] = nums[0];
+        dp[0][0] = 0;
+        dp[0][1] = nums[0];
 
-        for(int idx =1; idx < nums.length-1 ; idx++){
-                for(int p =0 ; p <=1 ; p++){
-                    int pick = Integer.MIN_VALUE; 
+        for (int idx = 1; idx < nums.length - 1; idx++) {
+            for (int p = 0; p <= 1; p++) {
+                int pick = Integer.MIN_VALUE;
 
-                    if(p == 1){
-                       pick = dp2[idx-1][0] + nums[idx];
+                if (p == 1) {
+                    pick = dp[idx - 1][0] + nums[idx];
 
-                       
-                    }
-
-                  //  int notPick = Integer.MIN_VALUE;
-
-                   
-                    int notPick = dp2[idx-1][1];
-
-                    dp2[idx][p] = Math.max(pick,notPick);
                 }
+
+                //  int notPick = Integer.MIN_VALUE;
+
+                int notPick = dp[idx - 1][1];
+
+                dp[idx][p] = Math.max(pick, notPick);
+            }
         }
 
-        int ans22 = dp2[nums.length-2][1];
+        int ans22 = dp[nums.length - 2][1];
 
-        return Math.max(ans11,ans22);
-
-
-
-        
+        return Math.max(ans11, ans22);
 
     }
 
