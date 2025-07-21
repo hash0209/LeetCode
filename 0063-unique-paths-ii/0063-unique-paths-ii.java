d960f607b2b6af70c6c05e3d8c26e23a94191b6c
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-07-21 16:28:57 -0400

    Time: 1 ms (16.86%), Space: 42.1 MB (27.65%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,35 +1,58 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
 
+        if(obstacleGrid[obstacleGrid.length-1][obstacleGrid[0].length-1] == 1 || 
+        obstacleGrid[0][0] == 1){
+            return  0 ;
+        }
+
         int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
-        for(int[] row : dp){
-            Arrays.fill(row , -1);
+
+        dp[0][0] = 1;
+
+        for(int r = 0; r < obstacleGrid.length ; r++ ){
+
+
+            for(int c = 0; c < obstacleGrid[0].length ; c++){
+
+                if( r== 0 && c== 0){
+                    continue ;
+                }
+
+
+                if(obstacleGrid[r][c] == 1){
+                    dp[r][c] = 0;
+                }
+                else{
+
+
+                int up = 0;
+
+                if(r-1 >=0){
+                 up = dp[r-1][c];
+                }
+
+                int left = 0;
+
+                if(c-1 >= 0){
+                 left = dp[r][c-1];
+                }
+
+                dp[r][c] = up + left ;
+            }
+            }
+
         }
-        return find(obstacleGrid , obstacleGrid.length -1 , obstacleGrid[0].length -1 , dp );
+
+
+        return dp[obstacleGrid.length-1][obstacleGrid[0].length-1];
+
+
+
+
+
+      
     }
 
-    public int find(int[][] nums , int  r, int c , int[][] dp){
 
-        if( r < 0 || c < 0 || nums[r][c] == 1){
-            return 0;
-        }
-
-        if( r== 0 && c == 0){
-            return 1;
-        }
-
-        if(dp[r][c]!=-1){
-            return dp[r][c];
-        }
-
-
-
-
-        
-        int up = find(nums , r-1 , c , dp);
-
-        int left = find(nums , r , c-1 , dp);
-
-        return  dp[r][c] = up + left ;
-    }
 }
