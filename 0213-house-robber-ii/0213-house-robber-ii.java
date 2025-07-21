77a565f1bbcf6da92674c49a4814d3d69b57429e
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-07-21 11:33:33 -0400

    Time: 0 ms (100%), Space: 41.5 MB (17.62%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,51 +1,39 @@
 class Solution {
     public int rob(int[] nums) {
 
-        if(nums.length == 1){
-              return nums[0];
+        if (nums.length == 1) {
+            return nums[0];
         }
 
-
-      
-
-
-        return Math.max(robb(nums , 0 , nums.length -2),robb(nums , 1 , nums.length-1));
+        return Math.max(robb(nums, 0, nums.length - 2), robb(nums, 1, nums.length - 1));
     }
 
-
-
-    public int robb(int[] nums , int start , int end ){
-
+    public int robb(int[] nums, int start, int end) {
 
         int prev2 = 0;
         int prev = nums[start];
 
-
-        for(int idx =start+1 ; idx <= end ; idx++){
-           
+        for (int idx = start + 1; idx <= end; idx++) {
 
             int pick = nums[idx];
 
-            if(idx- 2 >= 0){
-             pick = pick+ prev2;
-            }
+           // if (idx - 2 >= 0) {
+                pick = pick + prev2;
+           // }
 
             int notPick = 0;
 
-            if(idx -1 >= 0){
+           // if (idx - 1 >= 0) {
 
-             notPick = prev;
-            }
-
+                notPick = prev;
+          //  }
 
             prev2 = prev;
-            prev = Math.max(pick , notPick);
-
-
+            prev = Math.max(pick, notPick);
 
         }
 
         return prev;
-        
+
     }
 }
