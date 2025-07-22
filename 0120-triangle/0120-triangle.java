29407e27808caf9c912f1655a8b526feff273ab1
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-07-22 16:12:24 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,57 +1,33 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-
-     List<Integer> dp = new ArrayList<>();
-
-     dp.add(triangle.get(0).get(0));
-
-       
-
-     
-
-
-      
-
-
-        for(int row = 1 ; row < triangle.size() ; row ++){
-
-            List<Integer> temp = new ArrayList<>(triangle.get(row));
-            List<Integer> rows = triangle.get(row);
-            for (int idx = 0; idx < rows.size();idx ++){
-
-
-               int left = (idx  < triangle.get(row-1).size()) ?  dp.get(idx) :  Integer.MAX_VALUE;
-
-               int right = (idx-1 >= 0) ?  dp.get(idx-1) :  Integer.MAX_VALUE;
-
-               int ans = Math.min(left , right) + rows.get(idx);
-
-                temp.set(idx , ans);
-                
-            }
-
-            dp = temp ; 
-        }
-
-
-
-
-
-        
-       
-
-       
-
-       int ans = Integer.MAX_VALUE;
-
-       for (int idx =0 ; idx < dp.size(); idx++){
-                ans = Math.min(ans , dp.get(idx) );
-       }
-
-       return ans ;
+        return  find(triangle , 0, 0);
     }
 
 
 
+    public int find(List<List<Integer>> t , int row , int idx){
 
+       
+
+        if(row  ==  t.size() -1 ){
+            return t.get(row).get(idx);
+        }
+
+       
+
+
+
+        
+       int left = find(t , row+1 , idx);
+        
+
+
+        
+        int right = find(t, row+1 , idx+1) ;
+
+        
+
+        return Math.min(left , right) +  t.get(row).get(idx) ;
+
+    }
 }
