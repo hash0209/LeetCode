64309b21783843ad8076c81a1d902704ae966741
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-07-22 16:18:39 -0400

    Time: 4 ms (38.79%), Space: 45.7 MB (5.27%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,16 +1,42 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        return  find(triangle , 0, 0);
+
+        List<List<Integer>> dp = new ArrayList<>();
+
+        for(List<Integer> list : triangle ){
+           
+           dp.add(new ArrayList<>(list));
+           
+        }
+
+        for(List<Integer> list : dp){
+
+            for(int i = 0; i < list.size() ; i++){
+                list.set(i , Integer.MAX_VALUE);
+            }
+        }
+       
+
+       
+
+        return  find(triangle , 0, 0 , dp);
     }
 
 
 
-    public int find(List<List<Integer>> t , int row , int idx){
+    public int find(List<List<Integer>> t , int row , int idx ,   List<List<Integer>> dp){
 
-       
+        if(idx >=  t.get(row).size()){
+            return  Integer.MAX_VALUE;
+        }
 
         if(row  ==  t.size() -1 ){
             return t.get(row).get(idx);
+        }
+
+
+        if(dp.get(row).get(idx)!=Integer.MAX_VALUE){
+            return dp.get(row).get(idx);
         }
 
        
@@ -18,16 +44,20 @@ class Solution {
 
 
         
-       int left = find(t , row+1 , idx);
+       int left = find(t , row+1 , idx , dp);
         
 
 
         
-        int right = find(t, row+1 , idx+1) ;
+        int right = find(t, row+1 , idx+1 , dp) ;
+
+        int ans = Math.min(left , right) +  t.get(row).get(idx);
+
+        dp.get(row).set(idx , ans);
 
         
 
-        return Math.min(left , right) +  t.get(row).get(idx) ;
+        return ans ;
 
     }
 }
