05116c4ea5387a0b7fc365b29b4b2d7c3a2ee498
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-07-24 16:57:20 -0400

    Time: 17 ms (45.7%), Space: 45.7 MB (10.41%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,44 +1,72 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(amount == 0){
+        if (amount == 0) {
             return 0;
         }
 
-        int[][] dp = new int[coins.length][amount+1];
-        for(int[] row :dp){
-            Arrays.fill(row,-1);
+        int[][] dp = new int[coins.length][amount + 1];
+
+        for (int idx = 0; idx < coins.length; idx++) {
+            dp[idx][0] = 0;
         }
-        int ans = find(coins.length-1, amount , coins ,dp);
-                 return (ans == Integer.MAX_VALUE )?-1 : ans;
+
+        for (int amt = 1; amt <= amount; amt++) {
+            if (amt >= coins[0] && (amt % coins[0]) == 0) {
+                dp[0][amt] = (amt / coins[0]);
+            }
+            else{
+            dp[0][amt] = Integer.MAX_VALUE;
+            }
+        }
+
+        for (int idx = 1; idx < coins.length; idx++) {
+
+            for (int amt = 1; amt <= amount; amt++) {
+
+                int pick = Integer.MAX_VALUE;
+
+                if (amt >= coins[idx]) {
+                    pick = dp[idx][ amt - coins[idx]];
+                    pick = (pick == Integer.MAX_VALUE) ? pick : pick + 1;
+                }
+
+                int notPick = dp[idx-1][amt];
+
+                 dp[idx][amt] = Math.min(pick, notPick);
+
+            }
+        }
+
+        int ans = dp[coins.length-1][amount];
+        return (ans == Integer.MAX_VALUE) ? -1 : ans;
     }
 
-    public int find(int idx, int amt, int[] coins , int[][] dp) {
-        if(amt == 0){
+    public int find(int idx, int amt, int[] coins, int[][] dp) {
+        if (amt == 0) {
             return 0;
         }
         if (idx == 0) {
 
-            if(amt >= coins[0] && (amt % coins[0] )==0){
-                return (amt/coins[0]);
+            if (amt >= coins[0] && (amt % coins[0]) == 0) {
+                return (amt / coins[0]);
             }
             return Integer.MAX_VALUE;
-           
-          
+
         }
-        if(dp[idx][amt]!=-1){
+        if (dp[idx][amt] != -1) {
             return dp[idx][amt];
         }
 
         int pick = Integer.MAX_VALUE;
 
         if (amt >= coins[idx]) {
-            pick = find(idx, amt - coins[idx], coins , dp);
-            pick = (pick==Integer.MAX_VALUE)? pick : pick+1;
+            pick = find(idx, amt - coins[idx], coins, dp);
+            pick = (pick == Integer.MAX_VALUE) ? pick : pick + 1;
         }
 
-        int notPick = find(idx - 1, amt ,  coins , dp);
+        int notPick = find(idx - 1, amt, coins, dp);
 
-        return dp[idx][amt] = Math.min(pick,notPick);
+        return dp[idx][amt] = Math.min(pick, notPick);
 
     }
 }
