a90d92a9380b57e8f99644cb7634d96fcac7227a
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-07-26 16:48:03 -0400

    Time: 61 ms (21.37%), Space: 45.1 MB (95.2%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -4,22 +4,24 @@ class Solution {
         StringBuilder s1 = new StringBuilder(s);
         String s2 = s1.reverse().toString();
 
-        int[][] dp = new int[s.length()+1][s2.length()+1];
+        int[] dp = new int[s2.length()+1];
 
         for(int i =1 ; i <= s.length() ; i++){
+            int[] temp = new int[s2.length()+1];
 
             for(int j =1 ; j <= s2.length(); j++){
                  if(s.charAt(i-1)==s2.charAt(j-1)){
-                    dp[i][j] = 1+ dp[i-1][j-1];
+                    temp[j] = 1+ dp[j-1];
                  }
                  else{
-                    dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
+                    temp[j] = Math.max(temp[j-1],dp[j]);
                  }
 
             }
+            dp = temp;
         }
 
-        return dp[s.length()][s2.length()];
+        return dp[s2.length()];
         
     }
 }
