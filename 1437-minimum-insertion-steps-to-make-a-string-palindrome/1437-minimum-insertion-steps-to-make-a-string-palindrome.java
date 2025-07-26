30ace7f17742c3859f1b92db0209d87b41529648
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-07-26 17:17:37 -0400

    Time: 28 ms (34.35%), Space: 45.4 MB (67.44%) - LeetHub

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minInsertions(String s) {
+
+
+
+        int[][] dp = new int[s.length()+1][s.length()+1];
+
+
+        for(int i =1 ; i <= s.length() ; i++){
+            for(int j = 1 ; j <= s.length(); j ++){
+                if(s.charAt(i-1)==s.charAt(s.length()-j)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+
+
+        return s.length()-dp[s.length()][s.length()];
+        
+    }
+}
