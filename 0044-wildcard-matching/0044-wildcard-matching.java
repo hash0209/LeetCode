d9042d9b5428441e88f9a2cab6096a1bd2afabd0
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-07-27 14:40:01 -0400

    Time: 18 ms (66.97%), Space: 48.4 MB (31.57%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,10 +1,12 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        Integer[][] dp = new Integer[s.length()+1][p.length()+1];
-       return find(s.length(),p.length(),s,p  , dp);
+
+    Integer[][] dp = new Integer[s.length()+1][p.length()+1];
+      
+       return find(s.length(),p.length(),s,p,dp);
     }
 
-    public boolean find(int i, int j, String s, String p , Integer[][] dp) {
+    public boolean find(int i, int j, String s, String p ,Integer[][] dp) {
 
         boolean ans = false;
 
@@ -19,46 +21,31 @@ class Solution {
         if (i == 0) {
 
             while (j > 0) {
-                if (p.charAt(j - 1) == '*') {
-                    j--;
-                } else {
-                    return false;
-                }
+                if (p.charAt(j - 1) != '*') {
+                   return false;
+                } 
+                j--;
             }
             return true;
             
 
         }
+
         if(dp[i][j]!=null){
-            return dp[i][j] == 1 ? true : false;
+           return  dp[i][j] ==1 ? true : false;
         }
 
-        if (p.charAt(j - 1) == s.charAt(i - 1)) {
-            ans = find(i - 1, j - 1, s, p,dp);
+        if (p.charAt(j - 1) == s.charAt(i - 1) || p.charAt(j - 1)=='?' ) {
+            ans =  find(i - 1, j - 1, s, p,dp);
         }
 
-     else   if (p.charAt(j - 1)=='?') {
-            ans = find(i - 1, j - 1, s, p , dp);
-        }
-
-      else  if (p.charAt(j - 1) == '*' ){
-            boolean res = false;
-
-            for (int t = i; t >= 0; t--) {
-                if (res) {
-                    break;
-                }
-                res = find(t , j-1, s, p , dp);
-            }
-          ans =res;
+       else if (p.charAt(j - 1) == '*' ){
+           ans =  find(i-1,j,s,p,dp) || find(i,j-1,s,p,dp);
 
         }
-        if(ans){
-            dp[i][j] = 1;
-        }
-        else{
-            dp[i][j] =0;
-        }
+
+        dp[i][j] = (ans) ? 1 : 0;
+
 
         return ans;
 
