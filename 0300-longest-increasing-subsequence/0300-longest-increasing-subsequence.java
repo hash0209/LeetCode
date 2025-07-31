e2521dc86621ca13ce8d5a904105ce5a17e6d5a1
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-07-31 09:14:59 -0400

    Time: 131 ms (19.99%), Space: 44.6 MB (35.6%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,13 +1,41 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        Integer[][] dp = new Integer[nums.length][nums.length+1];
-        return find(nums.length-1 , nums.length, nums,dp);
+        int[] dp = new int[nums.length+1];
+
+        for(int last =0; last <= nums.length ; last++){
+            if(last == nums.length || nums[0] < nums[last]){
+                dp[last] =1 ;
+            }
+        }
+
+        for(int idx =1; idx < nums.length ; idx++){
+            int[] temp = new int[nums.length+1];
+
+            for(int last =0; last <= nums.length ; last++){
+
+                int pick = 0;
+                if(last == nums.length || nums[idx] < nums[last]){
+                    pick = dp[idx]+1;
+                }
+
+                int notpick = dp[last];
+
+               temp[last] = Math.max(pick,notpick);
+            }
+            dp =  temp;
+        }
+
+
+        return dp[nums.length];
     }
 
     public int find(int idx , int last  , int[] nums , Integer[][] dp){
-      if(idx < 0){
-        return 0;
-      }
+        if(idx == 0){
+            if(last == nums.length || nums[idx] < nums[last]){
+                return 1;
+            }
+            return 0;
+        }
 
         if( dp[idx][last]!=null){
             return dp[idx][last];
@@ -17,7 +45,7 @@ class Solution {
 
     
 
-        if( last == nums.length ||  nums[idx] <  nums[last]){
+        if(last == nums.length || nums[idx] <  nums[last]){
             pick = find(idx-1 , idx , nums ,dp)+1;
         }
 
