e2443512d89d3b4cfad4cd8d81dbbe7d14fb1e54
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-07-31 08:30:30 -0400

    Time: 151 ms (19.3%), Space: 79.3 MB (13.16%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,7 +1,30 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        Integer[][] dp = new Integer[nums.length][nums.length+1];
-        return find(nums.length-1 , nums.length, nums,dp);
+        int[][] dp = new int[nums.length][nums.length+1];
+
+        for(int last =0; last <= nums.length ; last++){
+            if(last == nums.length || nums[0] < nums[last]){
+                dp[0][last] =1 ;
+            }
+        }
+
+        for(int idx =1; idx < nums.length ; idx++){
+
+            for(int last =0; last <= nums.length ; last++){
+
+                int pick = 0;
+                if(last == nums.length || nums[idx] < nums[last]){
+                    pick = dp[idx-1][idx]+1;
+                }
+
+                int notpick = dp[idx-1][last];
+
+                dp[idx][last] = Math.max(pick,notpick);
+            }
+        }
+
+
+        return dp[nums.length-1][nums.length];
     }
 
     public int find(int idx , int last  , int[] nums , Integer[][] dp){
