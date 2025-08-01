553cb33b6f85215117c4b4c0a0dfb2794f223e69
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-07-31 20:47:38 -0400

    Time: 94 ms (21.45%), Space: 77.4 MB (18.5%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -18,22 +18,5 @@ class Solution {
         return dp[0][0];
     }
 
-    public int find(int idx , int prev , int[] nums , Integer[][] dp){
-        if(idx == nums.length){
-            return 0;
-        }
-
-        if(dp[idx][prev+1]!=null){
-            return dp[idx][prev+1];
-        }
-
-        int pick = 0;
-
-
-        if(prev == -1 || nums[idx] > nums[prev]){
-            pick = find(idx+1,idx , nums,dp)+1;
-        }
-        int notpick = find(idx+1 , prev , nums,dp);
-        return dp[idx][prev+1] = Math.max(pick , notpick);
-    }
+ 
 }
