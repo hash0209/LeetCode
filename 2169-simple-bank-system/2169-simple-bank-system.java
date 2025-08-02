609ce9d23e1f7dfd4a484358cb9fe39e951b554e
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-02 18:57:15 -0400

    Time: 97 ms (69.86%), Space: 101.2 MB (69.14%) - LeetHub

---
+++ 2169-simple-bank-system/2169-simple-bank-system.java
@@ -0,0 +1,71 @@
+class Bank {
+
+
+    long[] bal ;
+
+    public Bank(long[] balance) {
+
+        bal = balance;
+        
+    }
+    
+    public boolean transfer(int account1, int account2, long money) {
+
+        boolean ans1 = isValid(account1);
+        boolean ans2 = isValid(account2);
+
+        if(ans1 && ans2){
+                if(has(account1,money)){
+                    bal[account1-1] = bal[account1-1]-money;
+                    bal[account2-1] = bal[account2-1]+money;
+                    return true;
+                }
+        }
+
+        return false;
+
+
+        
+    }
+    
+    public boolean deposit(int account, long money) {
+
+        if(isValid(account)){
+            bal[account-1] = bal[account-1]+money;
+            return true;
+        }
+        return false;
+        
+    }
+    
+    public boolean withdraw(int account, long money) {
+        if(isValid(account) && has(account , money)){
+            bal[account-1] = bal[account-1] - money;
+            return true;
+        }
+        return false;
+        
+    }
+
+    public boolean isValid(int n){
+        if(n-1 >= 0 && n-1 < bal.length){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean has(int n , long amt){
+        if(bal[n-1]>= amt){
+            return true;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank obj = new Bank(balance);
+ * boolean param_1 = obj.transfer(account1,account2,money);
+ * boolean param_2 = obj.deposit(account,money);
+ * boolean param_3 = obj.withdraw(account,money);
+ */
