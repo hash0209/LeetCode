534f43cb53ffe9d86bc0006c7ce58e2a4a5ea798
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-02 22:23:09 -0400

    Time: 7 ms (14.05%), Space: 45 MB (7.08%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,19 +1,26 @@
 class Solution {
     public boolean isPalindrome(int x) {
 
-        String s = String.valueOf(x);
-
-        StringBuilder sb = new StringBuilder(s);
-     
-
-        StringBuilder sb2 = sb.reverse();
-           
-
-        if(s.equals(sb2.toString())){
-            return true;
+        if(x < 0){
+            return false;
         }
 
+        Long reverse = 0L;
+
+        Long x1 = (long) x;
+
+        while (x1 != 0) {
+            Long digit = x1 % 10;
+            x1 = x1 / 10L;
+
+            reverse = (reverse * 10L) + digit;
+
+        }
+
+        if (reverse == x) {
+            return true;
+        }
         return false;
-        
+
     }
 }
