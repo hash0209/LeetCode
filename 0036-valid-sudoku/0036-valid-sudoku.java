6c96053188733c4ba6a5944585021861c22f9547
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-04 10:05:18 -0400

    Time: 1 ms (99.93%), Space: 44 MB (99.17%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -4,32 +4,51 @@ class Solution {
         boolean[][] row = new boolean[9][9];
         boolean[][] col = new boolean[9][9];
 
-        boolean[][] boxes = new boolean[9][9];
+        for (int i = 0; i < 9; i++) {
 
+            for (int j = 0; j < 9; j++) {
 
-        for(int i = 0; i < 9; i++){
-
-            for(int j =0 ; j < 9 ; j ++){
-
-                if(board[i][j] == '.'){
+                if (board[i][j] == '.') {
                     continue;
                 }
 
-
                 int num = board[i][j] - '1';
 
-                int box = ((i/3) * 3)+j/3;
-
-                if(row[i][num] || col[j][num] || boxes[box][num]){
+                if (row[i][num] || col[j][num]) {
                     return false;
                 }
-                row[i][num] = col[j][num] = boxes[box][num] = true;
+
+
+                row[i][num] = col[j][num] = true;
+
             }
 
         }
 
+        for (int rowStart = 0; rowStart < 3; rowStart++) {
+
+            for (int colStart = 0; colStart < 3; colStart++) {
+                boolean visited[] = new boolean[9];
+
+                for (int r = rowStart * 3; r < rowStart * 3 + 3; r++) {
+
+                    for (int c = colStart * 3; c < colStart * 3 + 3; c++) {
+
+                        if (board[r][c] == '.') {
+                            continue;
+                        }
+
+                        if (visited[board[r][c] -'1']) {
+                            return false;
+                        }
+                        visited[board[r][c] -'1'] = true;
+
+                    }
+                }
+            }
+        }
+
         return true;
 
-        
     }
 }
