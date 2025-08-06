de5a88f5b7aea473f727d043820e13f19842c423
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-06 15:22:12 -0400

    Time: 3 ms (82.27%), Space: 45.1 MB (43.7%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,71 +1,34 @@
 class Solution {
     public String reverseVowels(String s) {
-        boolean[] l = new boolean[26];
-        l['a'-'a'] = true;
-        l['e'-'a'] = true;
-         l['i'-'a'] = true;
-          l['o'-'a'] = true;
-           l['u'-'a'] = true;
 
-        char[] ch = s.toCharArray();
+        String vowels ="aeiouAEIOU" ;
 
+        char[] ch =s.toCharArray();
 
-           
+        int left =0;
+        int right = s.length()-1;
+        while(left < right){
+            while(left < right && vowels.indexOf(ch[left])==-1){
+                left++;
+            }
+
+            while(left < right && vowels.indexOf(ch[right])==-1){
+                right--;
+            }
+
+            if(left < right){
+                char temp =ch[left];
+                ch[left]=ch[right];
+                ch[right]=temp;
+                left++;
+                right--;
+
+            }
+        }
+
+        return new String(ch);
+
 
         
-        int left = 0;
-        int right = ch.length-1;
-
-        while(left < right){
-
-           boolean isLeft = Character.isLetter(ch[left]);
-           boolean isRight = Character.isLetter(ch[right]);
-
-           if(isLeft && isRight){
-
-           
-
-            char tempLeft = Character.toLowerCase(ch[left]);
-            char tempright =Character.toLowerCase(ch[right]);
-
-            if(l[tempLeft-'a'] && l[tempright-'a']){
-                  char temp = ch[left];
-                  ch[left] = ch[right];
-                  ch[right] = temp ;
-                  left++;
-                  right--;
-            }
-            else if(l[tempLeft-'a']){
-                right--;
-            }
-            else if(l[tempright-'a']){
-                left++;
-            }
-            else{
-                left++;
-                right--;
-            }
-           }
-           else if(isRight){
-            left ++;
-           }
-           else if(isLeft){
-            right--;
-           }
-           else{
-            left++;
-            right--;
-           }
-
-            
-        }
-
-        StringBuilder sb = new StringBuilder();
-
-        for(int i =0; i < ch.length;i++){
-            sb.append(ch[i]);
-        }
-        return sb.toString();
-
     }
 }
