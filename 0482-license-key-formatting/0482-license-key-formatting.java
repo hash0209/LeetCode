d0905c1c1b40d30fedcbcc7c09a9f4f3eb43da7c
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-06 17:25:44 -0400

    Time: 18 ms (32.22%), Space: 48.1 MB (5.42%) - LeetHub

--- 0482-license-key-formatting/0482-license-key-formatting.java
+++ 0482-license-key-formatting/0482-license-key-formatting.java
@@ -1,25 +1,48 @@
 class Solution {
     public String licenseKeyFormatting(String s, int k) {
-        StringBuilder sb = new StringBuilder();
+
         int count = 0;
+        List<StringBuilder> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        for(int i =s.length()-1; i >=0 ; i--){
+            if(s.charAt(i) == '-'){
+                continue;
+            }
+            if(Character.isDigit(s.charAt(i))){
+                sb.append(s.charAt(i));
+            }
+            else{
+                 sb.append(Character.toUpperCase(s.charAt(i)));   
+            }
 
-        for (int i = s.length() - 1; i >= 0; i--) {
-            char c = s.charAt(i);
-            if (c == '-') continue; // skip dashes
-
-            sb.append(Character.toUpperCase(c));
+            
             count++;
 
-            if (count % k == 0) {
-                sb.append('-');
+            if(count == k){
+                list.add(new StringBuilder(sb));
+                sb=new StringBuilder();
+                count =0;
+            }
+
+        }
+
+        if(count !=0){
+            list.add(sb);
+        }
+
+        StringBuilder sb2 = new StringBuilder();
+        for(int i = list.size()-1; i >= 0 ; i--){
+            if( i == 0){
+                sb2.append(list.get(i).reverse());
+            }
+            else{
+                sb2.append(list.get(i).reverse() + "-");
             }
         }
 
-        // Remove trailing dash if present
-        if (sb.length() > 0 && sb.charAt(sb.length() - 1) == '-') {
-            sb.deleteCharAt(sb.length() - 1);
-        }
+        return sb2.toString();
 
-        return sb.reverse().toString();
+
+        
     }
 }
