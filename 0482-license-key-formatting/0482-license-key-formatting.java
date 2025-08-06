27fc0c36ffdb6a83f5739ad8f4bdaaf4b0ba6da4
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-06 17:36:03 -0400

    Time: 12 ms (60.91%), Space: 44.7 MB (74.27%) - LeetHub

--- 0482-license-key-formatting/0482-license-key-formatting.java
+++ 0482-license-key-formatting/0482-license-key-formatting.java
@@ -1,48 +1,35 @@
 class Solution {
     public String licenseKeyFormatting(String s, int k) {
 
-        int count = 0;
-        List<StringBuilder> list = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        for(int i =s.length()-1; i >=0 ; i--){
-            if(s.charAt(i) == '-'){
+       int count = 0;
+
+       StringBuilder sb = new StringBuilder();
+
+       for(int i =s.length()-1; i >= 0;i--){
+            char c = s.charAt(i);
+            if(c =='-'){
                 continue;
             }
-            if(Character.isDigit(s.charAt(i))){
-                sb.append(s.charAt(i));
+
+            if(Character.isDigit(c)){
+                sb.append(c);
             }
             else{
-                 sb.append(Character.toUpperCase(s.charAt(i)));   
+                sb.append(Character.toUpperCase(c));
             }
-
-            
             count++;
-
-            if(count == k){
-                list.add(new StringBuilder(sb));
-                sb=new StringBuilder();
+            if(count  == k){
+                sb.append("-");
                 count =0;
+
             }
+       }
 
-        }
-
-        if(count !=0){
-            list.add(sb);
-        }
-
-        StringBuilder sb2 = new StringBuilder();
-        for(int i = list.size()-1; i >= 0 ; i--){
-            if( i == 0){
-                sb2.append(list.get(i).reverse());
-            }
-            else{
-                sb2.append(list.get(i).reverse() + "-");
-            }
-        }
-
-        return sb2.toString();
+       if(count ==0 && !sb.isEmpty()){
+        sb.deleteCharAt(sb.length()-1);
+       }
 
 
-        
+        return sb.reverse().toString();
     }
 }
