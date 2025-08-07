fcc585cec5cc6a7455e61b3ec0bed448541fc921
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-07 12:41:56 -0400

    Time: 15 ms (26.41%), Space: 44.5 MB (8.18%) - LeetHub

---
+++ 1084-find-k-length-substrings-with-no-repeated-characters/1084-find-k-length-substrings-with-no-repeated-characters.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int numKLenSubstrNoRepeats(String s, int k) {
+
+        int l = 0;
+        int r = 0;
+        int count = 0;
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        while (r < s.length()) {
+
+            while (map.containsKey(s.charAt(r))) {
+                map.remove(s.charAt(l));
+                l++;
+            }
+
+            map.put(s.charAt(r), r);
+
+            if (map.size() > k) {
+                map.remove(s.charAt(l));
+                l++;
+
+            }
+            if (map.size() == k) {
+                count++;
+            }
+
+            r++;
+
+        }
+        return count;
+
+    }
+}
