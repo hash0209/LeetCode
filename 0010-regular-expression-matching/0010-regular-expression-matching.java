78b8f0b88ebf4cb9859ea9a7cbadc4a9c3d50a2c
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-11 12:40:01 -0400

    Time: 13 ms (26.53%), Space: 42 MB (76.78%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        return match(s.length(), p.length(), s, p);
+    }
+
+    public boolean match(int i, int j, String s, String p) {
+        if (i == 0 & j ==0 ) {
+            return true;
+        }
+        if (j == 0) {
+            return false;
+        }
+
+        if (i == 0) {
+            while (j >= 1 && p.charAt(j - 1) == '*') {
+                j=j-2;
+            }
+            return (j == 0);
+
+        }
+
+        if (p.charAt(j - 1) == s.charAt(i - 1) || p.charAt(j - 1) == '.') {
+            return match(i - 1, j - 1, s, p);
+        }
+        if (p.charAt(j - 1) == '*') {
+             boolean ans1  = match(i,j-2,s,p);
+
+             boolean ans2 = false;
+
+             if(p.charAt(j-2) == s.charAt(i - 1) || p.charAt(j - 2) == '.'){
+             
+              ans2 =match(i-1,j,s,p);
+             }
+             
+             return ans1 || ans2;
+        }
+        return false;
+    }
+}
