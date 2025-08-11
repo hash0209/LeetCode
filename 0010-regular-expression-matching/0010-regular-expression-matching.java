1ee28ab6a40cec2fe9c854b2f14089980bbfe9df
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-11 12:49:06 -0400

    Time: 1 ms (100%), Space: 42.3 MB (36.17%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,10 +1,12 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        return match(s.length(), p.length(), s, p);
+
+        Integer[][] dp = new Integer[s.length() + 1][p.length() + 1];
+        return match(s.length(), p.length(), s, p, dp);
     }
 
-    public boolean match(int i, int j, String s, String p) {
-        if (i == 0 & j ==0 ) {
+    public boolean match(int i, int j, String s, String p, Integer[][] dp) {
+        if (i == 0 & j == 0) {
             return true;
         }
         if (j == 0) {
@@ -13,27 +15,38 @@ class Solution {
 
         if (i == 0) {
             while (j >= 1 && p.charAt(j - 1) == '*') {
-                j=j-2;
+                j = j - 2;
             }
             return (j == 0);
 
         }
+        if (dp[i][j] != null) {
+            return (dp[i][j] == 1);
+        }
+
+        boolean res = false;
 
         if (p.charAt(j - 1) == s.charAt(i - 1) || p.charAt(j - 1) == '.') {
-            return match(i - 1, j - 1, s, p);
-        }
-        if (p.charAt(j - 1) == '*') {
-             boolean ans1  = match(i,j-2,s,p);
 
-             boolean ans2 = false;
+            res = match(i - 1, j - 1, s, p, dp);
 
-             if(p.charAt(j-2) == s.charAt(i - 1) || p.charAt(j - 2) == '.'){
-             
-              ans2 =match(i-1,j,s,p);
-             }
-             
-             return ans1 || ans2;
+        } else if (p.charAt(j - 1) == '*') {
+            boolean ans1 = match(i, j - 2, s, p, dp);
+
+            boolean ans2 = false;
+
+            if (p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.') {
+
+                ans2 = match(i - 1, j, s, p, dp);
+            }
+
+            res = ans1 || ans2;
         }
-        return false;
+        if (res) {
+            dp[i][j] = 1;
+        } else {
+            dp[i][j] = 0;
+        }
+        return res;
     }
 }
