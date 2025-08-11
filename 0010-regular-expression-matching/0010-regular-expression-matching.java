8385e35319b356c296a4bc97babc79fad92beeb8
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-11 12:53:31 -0400

    Time: 14 ms (26.41%), Space: 41.7 MB (97.65%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,11 +1,9 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-
-        Integer[][] dp = new Integer[s.length() + 1][p.length() + 1];
-        return match(s.length(), p.length(), s, p, dp);
+        return match(s.length(), p.length(), s, p);
     }
 
-    public boolean match(int i, int j, String s, String p, Integer[][] dp) {
+    public boolean match(int i, int j, String s, String p) {
         if (i == 0 & j == 0) {
             return true;
         }
@@ -20,32 +18,22 @@ class Solution {
             return (j == 0);
 
         }
-        if (dp[i][j] != null) {
-            return (dp[i][j] == 1);
-        }
 
         boolean res = false;
 
         if (p.charAt(j - 1) == s.charAt(i - 1) || p.charAt(j - 1) == '.') {
-
-            res = match(i - 1, j - 1, s, p, dp);
-
+            res = match(i - 1, j - 1, s, p);
         } else if (p.charAt(j - 1) == '*') {
-            boolean ans1 = match(i, j - 2, s, p, dp);
+            boolean ans1 = match(i, j - 2, s, p);
 
             boolean ans2 = false;
 
             if (p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.') {
 
-                ans2 = match(i - 1, j, s, p, dp);
+                ans2 = match(i - 1, j, s, p);
             }
 
             res = ans1 || ans2;
-        }
-        if (res) {
-            dp[i][j] = 1;
-        } else {
-            dp[i][j] = 0;
         }
         return res;
     }
