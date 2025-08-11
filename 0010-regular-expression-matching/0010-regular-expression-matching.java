be073399bf7234324eec1883b0c26409978cea7e
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-11 12:57:31 -0400

    Time: 13 ms (26.53%), Space: 41.9 MB (87.37%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -4,7 +4,7 @@ class Solution {
     }
 
     public boolean match(int i, int j, String s, String p) {
-        if (i == 0 & j == 0) {
+        if (i == 0 & j ==0 ) {
             return true;
         }
         if (j == 0) {
@@ -12,29 +12,28 @@ class Solution {
         }
 
         if (i == 0) {
-            while (j >= 1 && p.charAt(j - 1) == '*') {
-                j = j - 2;
+            while (j > 0 && p.charAt(j - 1) == '*') {
+                j=j-2;
             }
             return (j == 0);
 
         }
 
-        boolean res = false;
-
         if (p.charAt(j - 1) == s.charAt(i - 1) || p.charAt(j - 1) == '.') {
-            res = match(i - 1, j - 1, s, p);
-        } else if (p.charAt(j - 1) == '*') {
-            boolean ans1 = match(i, j - 2, s, p);
-
-            boolean ans2 = false;
-
-            if (p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.') {
-
-                ans2 = match(i - 1, j, s, p);
-            }
-
-            res = ans1 || ans2;
+            return match(i - 1, j - 1, s, p);
         }
-        return res;
+        if (p.charAt(j - 1) == '*') {
+             boolean ans1  = match(i,j-2,s,p);
+
+             boolean ans2 = false;
+
+             if(p.charAt(j-2) == s.charAt(i - 1) || p.charAt(j - 2) == '.'){
+             
+              ans2 =match(i-1,j,s,p);
+             }
+             
+             return ans1 || ans2;
+        }
+        return false;
     }
 }
