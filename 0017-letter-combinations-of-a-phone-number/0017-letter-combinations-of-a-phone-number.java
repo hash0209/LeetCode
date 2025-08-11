2815b5f877373a3f45d25107dc795fd6a7f990b6
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-11 14:57:14 -0400

    Time: 1 ms (53.92%), Space: 42.3 MB (51.44%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,50 +1,50 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
 
-        if (digits.isEmpty()) {
-            return new ArrayList<>();
-        }
-
         Map<Character, List<String>> map = new HashMap<>();
+        map.put('2',Arrays.asList("a","b","c"));
+        map.put('3',Arrays.asList("d","e","f"));
+        map.put('4',Arrays.asList("g","h","i"));
+        map.put('5',Arrays.asList("j","k","l"));
+        map.put('6',Arrays.asList("m","n","o"));
+        map.put('7',Arrays.asList("p","q","r" ,"s"));
+        map.put('8',Arrays.asList("t","u","v"));
+        map.put('9',Arrays.asList("w","x","y","z"));
 
-        map.put('2', Arrays.asList("a", "b", "c"));
-        map.put('3', Arrays.asList("d", "e", "f"));
-        map.put('4', Arrays.asList("g", "h", "i"));
-        map.put('5', Arrays.asList("j", "k", "l"));
-        map.put('6', Arrays.asList("m", "n", "o"));
-        map.put('7', Arrays.asList("p", "q", "r", "s"));
-        map.put('8', Arrays.asList("t", "u", "v"));
-        map.put('9', Arrays.asList("w", "x", "y", "z"));
-
-        char[] ch = digits.toCharArray();
-
-        List<String> ans = map.get(ch[0]);
-
-        if (ch.length == 1) {
-            return ans;
+        List<String> res = new ArrayList<>();
+        if(digits.length() == 0){
+            return res;
         }
 
-    
-        for (int i = 1; i < ch.length; i++) {
-            List<String> curr = map.get(ch[i]);
+        res =map.get(digits.charAt(0));
 
-              ans = merge(ans,curr);
-            
+        for(int i =1 ; i < digits.length(); i++){
+              res = merge(res,map.get(digits.charAt(i)));
         }
 
-        return ans;
-
+        return res;
+        
     }
 
-    public List<String> merge(List<String> str1, List<String> str2) {
-        List<String> ans = new ArrayList<>();
+    public  List<String> merge(List<String> l1 , List<String> l2){
 
-        for (String s1 : str1) {
-            for (String s2 : str2) {
-                ans.add(s1.concat(s2));
+        List<String> res = new ArrayList<>();
+
+        StringBuilder sb ;
+
+        for(int i =0; i < l1.size() ;i ++){
+            
+           
+            for(int j =0; j < l2.size() ; j++){
+                sb = new StringBuilder();
+                sb.append(l1.get(i));
+                sb.append(l2.get(j));
+                res.add(sb.toString());
+
+
             }
         }
-        return ans;
-    }
 
+        return res ;
+    }
 }
