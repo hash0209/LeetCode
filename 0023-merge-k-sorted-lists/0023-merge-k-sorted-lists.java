d277c2cdc7b56e5ad4869445904f0f4acd3a2940
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-11 21:48:34 -0400

    Time: 162 ms (5.05%), Space: 45.2 MB (5.28%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+
+       
+        if(lists.length == 0){
+            return null;
+        }
+
+        ListNode l1 = lists[0];
+
+        for(int i =1 ; i < lists.length ; i++){
+
+            ListNode l2 =lists[i];
+            l1 = merge(l1 , l2);
+        }
+
+        return l1;
+        
+    }
+
+    public ListNode merge(ListNode l1 , ListNode l2){
+
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+
+        while(l1!=null && l2!=null){
+            if(l1.val <= l2.val){
+                dummy.next = l1;
+                l1 =l1.next;
+            }
+            else{
+                dummy.next = l2;
+                l2 = l2.next;
+            }
+            dummy =dummy.next;
+        }
+
+        while(l1!=null){
+            dummy.next =l1;
+            l1=l1.next;
+            dummy=dummy.next;
+        }
+
+        while(l2!=null){
+            dummy.next =l2;
+            l2=l2.next;
+            dummy=dummy.next;        
+    }
+
+    return temp.next ;
+}
+}
