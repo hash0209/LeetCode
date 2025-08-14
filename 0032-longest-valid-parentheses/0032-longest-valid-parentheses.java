0f2454b7aaf5be6e3764b551d230bbd195344355
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-14 15:17:31 -0400

    Time: 1 ms (99.56%), Space: 42.3 MB (91.87%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,6 +1,6 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        int leftCount = 0; int rightCount = 0;  int maxlen1 =0;
+        int leftCount = 0; int rightCount = 0;  int maxlen =0;
 
         for(int i =0 ; i <s.length() ;i++){
             char c = s.charAt(i);
@@ -13,7 +13,7 @@ class Solution {
             }
 
             if(leftCount == rightCount){
-                maxlen1 =Math.max(maxlen1,leftCount+rightCount);
+                maxlen =Math.max(maxlen,leftCount+rightCount);
             }
             if(rightCount > leftCount){
                 leftCount =0;
@@ -21,8 +21,7 @@ class Solution {
             }
         }
 
-         leftCount = 0;  rightCount = 0;  int maxlen2 =0;
-
+         leftCount = 0;  rightCount = 0; 
         for(int i =s.length()-1 ; i >=0 ;i--){
             char c = s.charAt(i);
             if(c=='('){
@@ -34,7 +33,7 @@ class Solution {
             }
 
             if(leftCount == rightCount){
-                maxlen2 =Math.max(maxlen2,leftCount+rightCount);
+                maxlen =Math.max(maxlen,leftCount+rightCount);
             }
             if(leftCount > rightCount){
                 leftCount =0;
@@ -42,7 +41,7 @@ class Solution {
             }
         }
 
-        return Math.max(maxlen1,maxlen2);
+        return maxlen;
 
 
 
