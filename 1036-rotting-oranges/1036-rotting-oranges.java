cf753b5eec9cdd34c9ae1934aed339847daa9d6d
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-14 10:07:54 -0400

    Time: 2 ms (66.67%), Space: 42.3 MB (86.59%) - LeetHub

---
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -0,0 +1,89 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+
+        int m = grid.length;
+        int n = grid[0].length;
+
+        Queue<Pair> q = new LinkedList<>();
+        boolean[][] visited = new boolean[m][n];
+
+        boolean isFresh = false;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    q.add(new Pair(i, j));
+                    visited[i][j] = true;
+                }
+                if (grid[i][j] == 1) {
+                    isFresh = true;
+                }
+            }
+        }
+
+        // no rotten oranges
+        if (q.isEmpty() && isFresh) {
+            return -1;
+        }
+        if (q.isEmpty() && !isFresh) {
+            return 0;
+        }
+
+        int mins = -1;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int u = 0; u < size; u++) {
+                Pair cell = q.poll();
+                int i = cell.i;
+                int j = cell.j;
+
+                if (i - 1 >= 0 && grid[i - 1][j] != 0 && !visited[i - 1][j] ) {
+
+                    q.add(new Pair(i - 1, j));
+                    visited[i - 1][j] = true;
+
+                }
+
+                if (i + 1 < m  && grid[i + 1][j] != 0&&  !visited[i + 1][j] ) {
+                    q.add(new Pair(i + 1, j));
+                    visited[i + 1][j] = true;
+                }
+
+                if (j - 1 >= 0 && grid[i][j - 1] != 0 && !visited[i][j - 1] ) {
+                    q.add(new Pair(i, j - 1));
+                    visited[i][j - 1] = true;
+                }
+
+                if (j + 1 < n  && grid[i][j + 1] != 0 && !visited[i][j + 1]) {
+
+                    q.add(new Pair(i, j + 1));
+                    visited[i][j + 1] = true;
+                }
+
+            }
+            mins++;
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && !visited[i][j]) {
+                    return -1;
+                }
+            }
+        }
+        return mins;
+
+    }
+}
+
+class Pair {
+    int i;
+    int j;
+
+    public Pair(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+}
