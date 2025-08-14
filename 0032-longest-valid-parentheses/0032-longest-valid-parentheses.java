0bc918b3894f23ea42516433fe1060a8c64f9966
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-14 14:47:20 -0400

    Time: 1 ms (99.56%), Space: 42.5 MB (85.25%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,31 +1,50 @@
 class Solution {
     public int longestValidParentheses(String s) {
+        int leftCount = 0; int rightCount = 0;  int maxlen1 =0;
 
-        Stack<Integer> st = new Stack<>();
-        int maxlength = 0;
-         st.push(-1);
+        for(int i =0 ; i <s.length() ;i++){
+            char c = s.charAt(i);
+            if(c=='('){
+                leftCount++;
 
-       for(int i =0; i<s.length() ; i++){
-             char c = s.charAt(i);
+            }
+            else{
+                rightCount++;
+            }
 
-             if(c =='('){
-                st.push(i);
-             }
-             else{
-                if(!st.isEmpty()){
-                    st.pop();
-                }
+            if(leftCount == rightCount){
+                maxlen1 =Math.max(maxlen1,leftCount+rightCount);
+            }
+            if(rightCount > leftCount){
+                leftCount =0;
+                rightCount=0;
+            }
+        }
 
-                if(st.isEmpty()){
-                    st.push(i);
-                }
-             }
-             maxlength = Math.max(maxlength , i-st.peek());
+         leftCount = 0;  rightCount = 0;  int maxlen2 =0;
 
-       }
-       return maxlength ;
+        for(int i =s.length()-1 ; i >=0 ;i--){
+            char c = s.charAt(i);
+            if(c=='('){
+                leftCount++;
+
+            }
+            else{
+                rightCount++;
+            }
+
+            if(leftCount == rightCount){
+                maxlen2 =Math.max(maxlen2,leftCount+rightCount);
+            }
+            if(leftCount > rightCount){
+                leftCount =0;
+                rightCount=0;
+            }
+        }
+
+        return Math.max(maxlen1,maxlen2);
 
 
-        
+
     }
 }
