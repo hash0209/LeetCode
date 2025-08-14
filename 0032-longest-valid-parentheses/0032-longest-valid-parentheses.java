59ed6dc87ec164017ce9a7fe98435292fb9bdecf
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-14 12:05:21 -0400

    Time: 5 ms (7.76%), Space: 44.6 MB (14.87%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,31 +1,31 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(-1); // base for valid substring calculation
-        int maxLen = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+        Stack<Integer> st = new Stack<>();
+        int maxlength = 0;
+         st.push(-1);
 
-            if (c == '(') {
-                // push index of '(' onto the stack
-                stack.push(i);
-            } else {
-                // c == ')'
-                if (!stack.isEmpty()) {
-                    stack.pop(); // pop matching '(' index
+       for(int i =0; i<s.length() ; i++){
+             char c = s.charAt(i);
+
+             if(c =='('){
+                st.push(i);
+             }
+             else{
+                if(!st.isEmpty()){
+                    st.pop();
                 }
 
-                if (stack.isEmpty()) {
-                    // no matching '(', set new base
-                    stack.push(i);
-                } else {
-                    // valid substring length = current_index - last unmatched '(' index
-                    maxLen = Math.max(maxLen, i - stack.peek());
+                if(st.isEmpty()){
+                    st.push(i);
                 }
-            }
-        }
+             }
+             maxlength = Math.max(maxlength , i-st.peek());
 
-        return maxLen;
+       }
+       return maxlength ;
+
+
+        
     }
 }
