1548d448c80b880c34ff40e161ecf8a43ac2e526
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-13 23:46:25 -0400

    Time: 24 ms (78.91%), Space: 45.7 MB (84.6%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
@@ -1,51 +1,56 @@
 class Solution {
     public int cherryPickup(int[][] grid) {
+          
+           Integer[][][] dp = new Integer[grid.length][grid[0].length][grid[0].length];
 
-        int[][][] dp = new int[grid.length][grid[0].length][grid[0].length];
-        for (int[][] row : dp) {
-            for (int[] col : row) {
-                Arrays.fill(col, -1);
-            }
-        }
-        return find(0, 0, grid[0].length - 1, grid, dp);
+           
+           return find(0 , 0 , grid[0].length-1 , grid,dp);
     }
 
-    public int find(int m1, int n1, int n2, int[][] grid, int[][][] dp) {
+    public int find(int i , int j1 , int j2 , int[][] grid , Integer[][][] dp  ){
 
-       
-
-        if (n1 < 0 || n1 >= grid[0].length || n2 < 0 || n2 >= grid[0].length) {
+        if(j1 < 0 || j2 < 0 || j1 >= grid[0].length || j2 >=grid[0].length){
             return Integer.MIN_VALUE;
         }
-         if(m1 >= grid.length){
-                return 0;
-         }
 
-        if (dp[m1][n1][n2] != -1) {
-            return dp[m1][n1][n2];
+        if( i == grid.length-1){
+            if(j1 ==j2){
+                return grid[i][j1];
+            }
+            else{
+                return grid[i][j1] + grid[i][j2];
+            }
         }
 
-        int ans = Integer.MIN_VALUE;
+        if(dp[i][j1][j2]!= null){
+            return dp[i][j1][j2];
+        }
 
-        for (int i = n1 - 1; i <= n1 + 1; i++) {
+        int maxPoints = Integer.MIN_VALUE;
 
-            for (int j = n2 - 1; j <= n2 + 1; j++) {
+        for(int k1 = -1 ; k1 <= 1 ; k1++){
 
-                int ans1 = find(m1 + 1, i, j, grid, dp);
-
-                if (ans1 != Integer.MIN_VALUE) {
-                    if (n1 == n2) {
-                        ans1 = ans1 + grid[m1][n1];
-                    } else {
-                        ans1 = ans1 + grid[m1][n1] + grid[m1][n2];
+            for(int k2 =-1 ; k2 <= 1 ; k2++){
+                  
+                  int ans = find(i+1 , j1+k1 , j2+k2 , grid , dp );
+                  if(ans!=Integer.MIN_VALUE){
+                    if(j1 == j2){
+                        ans= ans+ grid[i][j1];
                     }
-                }
+                    else{
+                        ans = ans + grid[i][j1] + grid[i][j2];
+                    }
 
-                ans = Math.max(ans, ans1);
+
+                  }
+
+                  maxPoints =Math.max(maxPoints,ans);
 
             }
-
         }
-        return dp[m1][n1][n2] = ans;
+
+        return dp[i][j1][j2] = maxPoints;
+
+        
     }
 }
