b425a7a56dedc74c40f7b8c8a4f6960a6f8646cf
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-13 21:51:01 -0400

    Time: 0 ms (100%), Space: 40.4 MB (60.73%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,59 +1,29 @@
 class Solution {
     public int uniquePaths(int m, int n) {
 
-
-       // int[][] dp = new int[m][n];
-
-       // dp[0][0]= 1; 
-
-
         int[] dp = new int[n];
-        dp[0]= 1;
-    
 
-        
+        dp[0] = 1;
 
-        int ans = 0;
-
-        for (int r  = 0; r < m ; r ++){
-
+        for (int i = 0; i < m; i++) {
             int[] temp = new int[n];
-            
-            for(int c = 0 ; c < n ; c ++){
 
-                if( r== 0 && c == 0){
-                    temp[0] = 1;
-                    
+            for (int j = 0; j < n; j++) {
+
+                if (i == 0 && j == 0) {
+                    temp[0] =1;
+                    continue;
                 }
-                else{
 
-                int up = 0;
+                int up = (i - 1) >= 0 ? dp[j] : 0;
+                int left = (j - 1) >= 0 ? temp[j - 1] : 0;
 
-                if(r -1 >= 0){
-                   up =  dp[c];
-                }
-                  
-
-                  int left = 0;
-                  if(c-1>=0){
-                     left =temp[c-1];
-                  }
-
-                 temp[c] = up+left;
-
-            }
+                temp[j] = up + left;
             }
 
-            dp = temp;
-            
-}
-
-       return dp[n-1];
+            dp =temp;
+        }
+        return dp[n - 1];
 
     }
-
-
-
-
-
 }
