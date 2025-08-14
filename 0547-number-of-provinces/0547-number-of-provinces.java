2a0b67620a010df80a4ea1471be1833238b734de
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-14 08:11:23 -0400

    Time: 3 ms (32.98%), Space: 47.1 MB (80.5%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -6,35 +6,53 @@ class Solution {
         int n = isConnected.length;
         boolean[] visited = new boolean[n];
 
-        for(int i =0; i < n ; i++){
-          
-                if(!visited[i]){
-                    ans ++;
-                    dfs(i,isConnected,visited);
-                     
-                }
+        List<List<Integer>> adj  =  build(isConnected);
+
+        for (int i = 0; i < adj.size(); i++) {
+
+            if (!visited[i]) {
+                ans++;
+                dfs(i, adj, visited);
+
             }
-        
+        }
 
         return ans;
-        
+
     }
 
+    public void dfs(int i,  List<List<Integer>> adj , boolean[] visited) {
+        if (visited[i]) {
+            return;
+        }
+
+        visited[i] = true;
+
+        for (int nb : adj.get(i)) {
+            
+                dfs(nb, adj, visited);
+            
+        }
+    }
+
+    public List<List<Integer>> build(int[][] mat){
+          List<List<Integer>> adj = new ArrayList<>();
+
+          
+          for(int i = 0; i < mat.length ; i++){
+            
+            List<Integer> list = new ArrayList<>();
+            for(int j =0; j < mat.length ; j ++){
+                if(i!=j && mat[i][j] == 1){
+                    list.add(j);
+                }
+            }
+            adj.add(list);
 
 
-    public void dfs (int i , int[][] mat , boolean[] visited){
-           if(visited[i]){
-            return ;
-           }     
+          }
+          return adj;
 
-           visited[i] = true;
-
-           
-
-           for(int j =0; j < mat[i].length ; j ++){
-              if(   mat[i][j] ==1){
-                dfs (j , mat , visited);
-              }
-           }
+          
     }
 }
