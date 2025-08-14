711333ca6d6be7cf1633670e9cffb28d40acd6b4
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-13 21:21:12 -0400

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Geek's Training/geeks-training.java
+++ Difficulty: Medium/Geek's Training/geeks-training.java
@@ -1,66 +1,35 @@
 // User function Template for Java
 
-
 class Solution {
     public int maximumPoints(int arr[][]) {
+        
+        int[][] dp = new int[arr.length][arr[0].length];
         // code here
+        dp[0][0] =arr[0][0];
+        dp[0][1]=arr[0][1];
+        dp[0][2] =arr[0][2];
         
-        
-        
-        int[] dp = new int[3];
-        
-        dp[0] = arr[0][0];
-        dp[1]= arr[0][1];
-        dp[2] = arr[0][2];
-        
-        
-        
-        for(int days = 1; days < arr.length ; days++){
+        for(int days =1 ; days< arr.length ; days++){
             
-            int[] temp = new int[3];
-            
-           for(int i =0 ; i < 3 ; i++){
-               
-               
-               int ans = Integer.MIN_VALUE;
-               
-               for(int choosen = 0; choosen < 3 ; choosen++){
-                   
-                   if(i!=choosen){
-                   
-                   ans = Math.max(ans , dp[choosen]);
-                   }
-                   
-               }
-               
-              temp[i] = ans + arr[days][i]; 
-               
-               
-               
-               
-           }
-           
-           dp =temp;
-   
+            for(int curr =0 ; curr < 3 ; curr++){
+                
+                int maxpoints =0;
+                
+                
+                for(int prev =0; prev < 3 ; prev ++){
+                    if(prev!= curr){
+                        maxpoints = Math.max(maxpoints , dp[days-1][prev]);
+                    }
+                }
+                dp[days][curr] = maxpoints + arr[days][curr];
+            }
+        }
         
-     
+       int max =0;
        
+       for(int i =0 ; i < 3 ; i++){
+           max = Math.max(max,dp[arr.length-1][i]);
+       }
+       return max;
     }
-    
-     int ans = Integer.MIN_VALUE;
-    
-    
-    
-    for(int i = 0; i < 3 ; i++){
-     
-        
-          ans = Math.max(dp[i],ans);
-        
-    }
-    
-    return ans;
-    
-    
-  
-}
 }
