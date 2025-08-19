2ca6149570ee0309072894100201220ce1ae9e2d
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-19 18:36:50 -0400

    Time: 55 ms (96.36%), Space: 41.2 MB (44.16%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,77 +1,68 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-        solve(0,0,board);
-    }
 
+        boolean[][] rows= new boolean[9][10];
+        boolean[][] cols = new boolean[9][10];
+        boolean[][] boxes = new boolean[9][10];
 
-
-    public boolean solve(int row , int col , char[][] board){
-       boolean ans = false;
-
-       if(row == 9){
-        return true;
-       }
-
-        if(col == 9){
-          return  solve(row+1, 0 , board);
-        }
-
-        if(board[row][col] != '.'){
-           return solve(row,col+1,board);
-        }
-
-        for( int i = '1' ; i <='9';i++){
-            if(isPossible(i , row , col , board)){
-               
-                board[row][col] = (char)i;
-              ans =   solve(row,col+1,board);
-              if(ans){
-                return true;
-              }
-              else{
-                board[row][col] = '.';
-              }
-
-            }
-        }
-
-        return false;
-    }
-
-    public boolean isPossible(int n , int row , int col , char[][] board){
-        //check column 
-
-        for(int i =0 ; i < 9 ; i++){
-            int val = board[i][col];
-            if(val == n){
-                return false;
-            }
-        }
-
-        //check row
-
-        for(int i =0 ; i < 9;i++){
-            int val = board[row][i];
-            if(val == n){
-                return false;
-            }
-        }
-
-        int rowDup = row - (row%3);
-        int colDup = col - (col%3);
-
-        for(int i = rowDup ; i < rowDup+3 ; i++){
-            for(int j = colDup ; j < colDup+3 ; j++){
-                int val = board[i][j];
-                if(val == n){
-                    return false;
+        for(int i = 0; i < board.length ; i++){
+            for(int j =0; j < board[0].length ; j++){
+                if(board[i][j] =='.'){
+                    continue;
                 }
+
+                int num = board[i][j]-'0';
+                int box = (i/3)*3 +(j/3);
+
+                rows[i][num] = cols[j][num] = boxes[box][num]= true;
+                
+
             }
         }
 
-        return true;
+        solve(0,0,board,rows,cols,boxes);
 
 
+        
+        
+
+       
+
+        
+    }
+
+    public boolean solve(int row , int idx , char[][] board , boolean[][]  rows , boolean[][] cols , boolean[][] boxes){
+
+                 if(idx == 9){
+                    return  solve(row+1 , 0 , board , rows , cols , boxes);
+                 }
+
+                 if(row == 9){
+                    return true;
+                 }
+
+                 if(Character.isDigit(board[row][idx])){
+                   return solve(row,idx+1,board,rows,cols,boxes);
+                 }
+
+                 int box = (row/3)*3 +(idx/3);
+
+                 for(int i =1 ; i <= 9; i ++){
+                     if(rows[row][i] || cols[idx][i]|| boxes[box][i]){
+                           continue;
+                     }
+                     rows[row][i] = cols[idx][i] = boxes[box][i]= true;
+                     board[row][idx] = (char) (i + '0');
+                    if(solve(row,idx+1,board,rows,cols,boxes)){
+                        return true;
+                    }
+                    rows[row][i] = cols[idx][i] = boxes[box][i]= false;
+                    board[row][idx] = '.';
+
+
+
+                 }
+                 return false;
 
     }
 }
