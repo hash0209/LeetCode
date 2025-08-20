91837c60ad64c3312cdcf188b12221484a3de5ed
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-19 22:47:32 -0400

    Time: 0 ms (100%), Space: 46.4 MB (49.85%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,40 +1,38 @@
 class Solution {
     public int trap(int[] height) {
-       
 
-       int lmax = 0;
-       int rmax = 0;
+        int lmax =0;
+        int rmax =0;
 
-       int l =0;
-       int r = height.length-1;
+        int l =0;
+        int r = height.length-1;
 
-       int total =0;
+        int area =0;
 
-       while(l < r){
-        if(height[l] <= height[r]){
-            if(lmax > height[l]){
-                total = total + lmax -height[l];
-
+        while(l < r){
+            if(height[l]< height[r]){
+                if(height[l] > lmax){
+                    lmax= height[l];
+                }
+                else{
+                    area += lmax-height[l];
+                }
+                l++;
             }
             else{
-                lmax =height[l];
+                if(height[r] > rmax){
+                    rmax = height[r];
+                }
+                else{
+                    area+= rmax-height[r];
+                }
+                r-- ;
+
             }
-            l++;
+
+
         }
-        else{
-            if(rmax > height[r]){
-                total = total + rmax -height[r];
-
-            }
-            else{
-                rmax =height[r];
-            }
-            r--;
-        }
-       }
-
-
-       return total;
-
+        return area ;
+        
     }
 }
