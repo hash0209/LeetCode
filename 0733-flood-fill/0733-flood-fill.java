3a9b986db2ace86e0643d9d7f032395005a5008d
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-20 16:05:42 -0400

    Time: 2 ms (9.05%), Space: 45.2 MB (19.36%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -14,38 +14,37 @@ class Solution {
         }
         
         int originalcolor = image[sr][sc];
-        boolean[][] seen = new boolean[image.length][image[0].length];
-        seen[sr][sc] = true;
+       
+      
         res[sr][sc] = color;
         Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(sr, sc));
         while (!q.isEmpty()) {
-            int size = q.size();
-            for (int k = 0; k < size; k++) {
+           
                 Pair curr = q.poll();
                 int i = curr.i;
                 int j = curr.j;
-                if (i - 1 >= 0 && !seen[i - 1][j] && image[i - 1][j] == originalcolor) {
+                if (i - 1 >= 0 &&  image[i - 1][j] == originalcolor && res[i - 1][j] != color) {
                     res[i - 1][j] = color;
-                    seen[i - 1][j] = true;
+                   
                     q.add(new Pair(i - 1, j));
                 }
-                if (i + 1 < m && !seen[i + 1][j] && image[i + 1][j] == originalcolor) {
+                if (i + 1 < m &&   image[i + 1][j] == originalcolor && res[i + 1][j] != color) {
                     res[i + 1][j] = color;
-                    seen[i + 1][j] = true;
+                  
                     q.add(new Pair(i + 1, j));
                 }
-                if (j - 1 >= 0 && !seen[i][j - 1] && image[i][j - 1] == originalcolor) {
+                if (j - 1 >= 0 &&  image[i][j - 1] == originalcolor && res[i][j - 1] != color) {
                     res[i][j - 1] = color;
-                    seen[i][j - 1] = true;
+                   
                     q.add(new Pair(i, j - 1));
                 }
-                if (j + 1 < n && !seen[i][j + 1] && image[i][j + 1] == originalcolor) {
+                if (j + 1 < n &&   image[i][j + 1] == originalcolor && res[i][j + 1] != color) {
                     res[i][j + 1] = color;
-                    seen[i][j + 1] = true;
+                   
                     q.add(new Pair(i, j + 1));
                 }
-            }
+            
 
         }
         return res;
