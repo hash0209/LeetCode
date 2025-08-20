6812b877d0ec35fa5d2bf93fc288cf7397c0a49e
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-20 16:10:44 -0400

    Time: 1 ms (44.33%), Space: 45 MB (50.02%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -4,47 +4,38 @@ class Solution {
         int n = image[0].length;
         int[][] res = new int[m][n];
 
-        for(int i =0; i < m ; i++){
-            for(int j =0; j < n ; j++){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 res[i][j] = image[i][j];
             }
         }
         if (image[sr][sc] == color) {
             return res;
         }
-        
+
         int originalcolor = image[sr][sc];
-       
-      
+
+        int[][] dir = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+
         res[sr][sc] = color;
         Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(sr, sc));
         while (!q.isEmpty()) {
-           
-                Pair curr = q.poll();
-                int i = curr.i;
-                int j = curr.j;
-                if (i - 1 >= 0 &&  image[i - 1][j] == originalcolor && res[i - 1][j] != color) {
-                    res[i - 1][j] = color;
-                   
-                    q.add(new Pair(i - 1, j));
+
+            Pair curr = q.poll();
+            int i = curr.i;
+            int j = curr.j;
+            for (int d = 0; d < dir.length; d++) {
+                int ni = i + dir[d][0];
+                int nj = j + dir[d][1];
+
+                if (ni >= 0 && nj >= 0 && ni < m && nj < n) {
+                    if (image[ni][nj] == originalcolor && res[ni][nj] != color) {
+                        res[ni][nj] = color;
+                        q.add(new Pair(ni, nj));
+                    }
                 }
-                if (i + 1 < m &&   image[i + 1][j] == originalcolor && res[i + 1][j] != color) {
-                    res[i + 1][j] = color;
-                  
-                    q.add(new Pair(i + 1, j));
-                }
-                if (j - 1 >= 0 &&  image[i][j - 1] == originalcolor && res[i][j - 1] != color) {
-                    res[i][j - 1] = color;
-                   
-                    q.add(new Pair(i, j - 1));
-                }
-                if (j + 1 < n &&   image[i][j + 1] == originalcolor && res[i][j + 1] != color) {
-                    res[i][j + 1] = color;
-                   
-                    q.add(new Pair(i, j + 1));
-                }
-            
+            }
 
         }
         return res;
