d8d9cadcfdb6fe663acfb15e6fc09865d8116adf
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-20 13:05:21 -0400

    Time: 27 ms (8.89%), Space: 44.6 MB (83.02%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,37 +1,65 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        Boolean[][] dp = new Boolean[s.length()+1][p.length()+1];
-        return match(s.length(),p.length(),s,p ,dp);
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+
+        dp[0][0] = true;
+        for (int i = 1; i < dp.length; i++) {
+            dp[i][0] = false;
+        }
+
+        for (int i = 0; i <= s.length(); i++) {
+            for (int j = 0; j <= p.length(); j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = true;
+                } else if (j == 0) {
+                    dp[i][j] = false;
+                }
+
+                else if (i == 0) {
+                    int j1 = j;
+                    while (j1 > 0 && p.charAt(j1 - 1) == '*') {
+                        j1--;
+                    }
+                    dp[i][j] = (j1 == 0);
+                } else {
+                    if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    } else if (p.charAt(j - 1) == '*') {
+                        dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                    }
+
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
     }
-    public boolean match(int i , int j , String s ,  String p , Boolean[][] dp){
+
+    public boolean match(int i, int j, String s, String p, Boolean[][] dp) {
 
         boolean ans = false;
 
-        if(i == 0 && j == 0){
+        if (i == 0 && j == 0) {
             return true;
         }
-        if( j== 0){
+        if (j == 0) {
             return false;
         }
-        if(i == 0){
-            while( j >0 && p.charAt(j-1) == '*'){
+        if (i == 0) {
+            while (j > 0 && p.charAt(j - 1) == '*') {
                 j--;
             }
             return j == 0;
         }
-        if(dp[i][j]!=null){
+        if (dp[i][j] != null) {
             return dp[i][j];
         }
-            
 
+        if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
+            ans = match(i - 1, j - 1, s, p, dp);
+        } else if (p.charAt(j - 1) == '*') {
+            ans = match(i, j - 1, s, p, dp) || match(i - 1, j, s, p, dp);
+        }
 
-            if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?'){
-                ans = match(i-1 , j-1 , s , p , dp);
-            }
-            else if(p.charAt(j-1) == '*'){
-                ans = match(i , j-1 , s , p,dp) || match(i-1,j,s,p,dp);
-            }
-
-            return dp[i][j] = ans;
+        return dp[i][j] = ans;
     }
 }
