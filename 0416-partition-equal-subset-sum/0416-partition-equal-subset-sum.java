2dc20c9da174e66cb70f4356f83ec9cc2e2c49eb
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-08-20 22:09:59 -0400

    Time: 45 ms (60.16%), Space: 44.8 MB (73.98%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,92 +1,41 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        
-     int sum = 0;
-
-     for(int i = 0; i < nums.length ; i++){
-        sum+=nums[i];
-     }
-
-     if(sum %2 == 1){
-        return false;
-     }
-
-     sum = sum/2;
-
-     int[][] dp = new int[nums.length][sum+1];
-
-    for(int idx = 0; idx < nums.length ; idx++){
-        dp[idx][0] =1;
-    }
-
-    for(int t = 1 ; t <= sum ; t ++){
-        if(nums[0]==t){
-            dp[0][t] = 1;
+        int sum = 0;
+        for(int i =0; i < nums.length ; i++){
+            sum += nums[i];
         }
-    }
 
+        if(sum%2!=0){
+            return false;
+        }
 
-    for(int idx = 1; idx < nums.length ; idx++){
-        
+        int target = sum/2;
 
-        for(int target =1 ; target <= sum ; target++){
-
-
-            int take = 0;
-
-            if(target >= nums[idx]){
-                take = dp[idx-1][target-nums[idx]];
+        boolean[] dp = new boolean[target+1];
+        dp[0]= true;
+        for(int t =1; t <= target ; t++){
+            if(nums[0]==t){
+                dp[t] =true;
             }
-
-            int notTake = dp[idx-1][target];
-
-            if(take == 1 || notTake ==1){
-                dp[idx][target] =1;
-              }
-              else{
-                 dp[idx][target] =0;
-              }
         }
-    }
+        for(int i =1 ; i< nums.length ; i++){
+             boolean[] temp = new boolean[target+1];
+             temp[0]=true;
+             for(int t =1; t<= target ;t++){
+                 boolean pick = false;
+                 if(t >= nums[i]){
+                    pick = dp[t-nums[i]];
+                 }
+                 boolean notpick = dp[t];
 
+                 temp[t] = pick || notpick;
+             }
+             dp = temp;
+        }
 
+        return dp[target];
 
-
-     return dp[nums.length-1][sum] ==1 ? true : false;
-    }
-
-    public boolean find(int[] nums , int idx , int target , int[][] dp){
-              if(target == 0){
-                return true;
-              }
-              if(idx == 0){
-                if(nums[0]== target){
-                    return true;
-                }
-                return false;
-              }
-
-              if(dp[idx][target]!=-1){
-                return dp[idx][target] ==1 ? true : false;
-              }
-
-
-
-              boolean take = false;
-
-              if(target >= nums[idx]){
-                take = find(nums , idx-1 , target-nums[idx] , dp);
-              }
-
-              boolean notTake = find(nums , idx-1 , target , dp);
-
-              if(take || notTake){
-                dp[idx][target] =1;
-              }
-              else{
-                 dp[idx][target] =0;
-              }
-
-              return   take || notTake;
+        
+        
     }
 }
