fcc2453e43112a3fb6651a6f8f5328f6e164f6f1
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-09-16 12:19:00 -0400

    Time: 1 ms (100%), Space: 44.5 MB (53.09%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -0,0 +1,72 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+          
+
+           ListNode temp = new ListNode(-1);
+            ListNode  head = temp;
+           int carry = 0;
+
+           while(l1!=null && l2!= null){
+                  int sum = l1.val+l2.val+carry;
+                  if(sum >= 10){
+                    carry =1;
+                    sum = sum-10;
+                  }
+                  else{
+                    carry =0;
+                  }   
+
+                  temp.next = new ListNode(sum);
+                  temp = temp.next; 
+                  l1 =l1.next;
+                  l2 =l2.next;
+           }
+
+           while(l1!=null){
+               int sum = l1.val+carry;
+               if(sum >= 10){
+                    carry =1;
+                    sum = sum-10;
+                  }
+                  else{
+                    carry =0;
+                  }   
+
+                  temp.next = new ListNode(sum);
+                  temp = temp.next;
+                  l1=l1.next;
+           }
+            while(l2!=null){
+               int sum = l2.val+carry;
+               if(sum >= 10){
+                    carry =1;
+                    sum = sum-10;
+                  }
+                  else{
+                    carry =0;
+                  }   
+
+                  temp.next = new ListNode(sum);
+                  temp = temp.next;
+                  l2=l2.next;
+           }
+
+           if(carry ==1){
+            temp.next = new ListNode(carry);
+                  temp = temp.next;
+           }
+           return head.next;
+           
+        
+    }
+}
