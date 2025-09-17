9651afe5163a54daa6344442626706b058f3e34b
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-09-17 12:35:30 -0400

    Time: 0 ms (100%), Space: 42.3 MB (38.74%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,48 +1,42 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-
-        Map<Character, List<String>> map = new HashMap<>();
-        map.put('2',Arrays.asList("a","b","c"));
-        map.put('3',Arrays.asList("d","e","f"));
-        map.put('4',Arrays.asList("g","h","i"));
-        map.put('5',Arrays.asList("j","k","l"));
-        map.put('6',Arrays.asList("m","n","o"));
-        map.put('7',Arrays.asList("p","q","r" ,"s"));
-        map.put('8',Arrays.asList("t","u","v"));
-        map.put('9',Arrays.asList("w","x","y","z"));
-
         List<String> res = new ArrayList<>();
+
         if(digits.length() == 0){
             return res;
         }
+        Map<Character,String> m = new HashMap<>();
+        m.put('2',"abc");
+        m.put('3',"def");
+        m.put('4',"ghi");
+        m.put('5',"jkl");
+        m.put('6',"mno");
+        m.put('7',"pqrs");
+        m.put('8',"tuv");
+        m.put('9',"wxyz");
 
-        res =map.get(digits.charAt(0));
-
-        for(int i =1 ; i < digits.length(); i++){
-              res = merge(res,map.get(digits.charAt(i)));
-        }
-
-        return res;
         
+        find(0,digits,m,new StringBuilder(),res);
+        return res;
+
+
+
     }
 
-    public  List<String> merge(List<String> l1 , List<String> l2){
-
-        List<String> res = new ArrayList<>();
-
-        StringBuilder sb ;
-
-        for(int i =0; i < l1.size() ;i ++){
-            
-           
-            for(int j =0; j < l2.size() ; j++){
-               
-                res.add(l1.get(i).concat(l2.get(j)));
-
-
-            }
+    public void find(int idx , String digits , Map<Character,String> m , StringBuilder curr , List<String> res){
+        if(idx == digits.length()){
+            res.add(curr.toString());
+            return;
         }
 
-        return res ;
+        String letters = m.get(digits.charAt(idx));
+
+        for(char c : letters.toCharArray()){
+            curr.append(c);
+            find(idx+1,digits , m , curr,res);
+            curr.deleteCharAt(curr.length()-1);
+
+
+        }
     }
 }
