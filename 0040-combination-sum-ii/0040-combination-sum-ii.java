776421f47c6c0413ca70fe4f49e10fc0b328f54a
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-09-18 06:56:05 -0400

    Time: 3 ms (72.23%), Space: 43.2 MB (87.47%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,35 +1,36 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(candidates);
-        find(candidates.length - 1, target, candidates, res, new ArrayList<>());
-        return res;
-        
 
+        find(candidates.length -1 , candidates,target,res,new ArrayList<>());
+        return res;
+
+
+        
     }
 
-    public void find(int idx, int target, int[] nums, List<List<Integer>> res, List<Integer> ds) {
-        if (target == 0) {
 
-            res.add(new ArrayList<>(ds));
-            return;
-        }
-        if (idx == -1) {
-            return;
-        }
+    public void find(int idx , int[] nums , int target , List<List<Integer>> res , List<Integer> ds){
+         if(target == 0){
+             res.add(new ArrayList<>(ds));
+             return;
+         }
 
-        if (target >= nums[idx]) {
+         if(idx < 0){
+            return ;
+         }
+
+         if(nums[idx] <= target){
             ds.add(nums[idx]);
-            find(idx - 1, target - nums[idx], nums, res, ds);
-            ds.remove(ds.size() - 1);
-        }
-        idx = idx - 1;
+            find(idx-1 , nums , target-nums[idx],res, ds);
+            ds.remove(ds.size()-1);
+         }
 
-        while (idx >= 0 && nums[idx] == nums[idx + 1]) {
+         idx = idx-1;
+         while(idx >=0 && nums[idx] == nums[idx+1]){
             idx--;
-        }
-
-        find(idx, target, nums, res, ds);
+         }
+         find(idx , nums , target , res , ds);
     }
 }
