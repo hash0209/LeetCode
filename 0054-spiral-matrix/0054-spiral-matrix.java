a885aaeff17428c2a18bde3e94955cd9db46e292
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-09-18 08:32:02 -0400

    Time: 0 ms (100%), Space: 41.5 MB (82.58%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,45 +1,47 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int rowStart = 0;
-        int rowEnd = matrix.length - 1;
-        int colStart = 0;
-        int colEnd = matrix[0].length - 1;
 
-        List<Integer> list = new ArrayList<>();
+        List<Integer> l = new ArrayList<>();
 
-        while (rowStart <= rowEnd && colStart <= colEnd) {
+        int rowStart =0;
+        int rowEnd = matrix.length-1;
+        int colStart =0;
+        int colEnd = matrix[0].length-1;
 
-            for (int i = colStart; i <= colEnd; i++) {
-                list.add(matrix[rowStart][i]);
+        while(rowStart <= rowEnd && colStart <= colEnd){
+
+            for(int i = colStart; i <= colEnd ; i++){
+                l.add(matrix[rowStart][i]);
+
             }
+
             rowStart++;
 
-            for (int i = rowStart; i <= rowEnd; i++) {
-                list.add(matrix[i][colEnd]);
+            for(int i = rowStart ; i <= rowEnd ; i++){
+                l.add(matrix[i][colEnd]);
             }
 
             colEnd--;
 
-            if (rowStart <= rowEnd) {
-
-                for (int i = colEnd; i >= colStart; i--) {
-                    list.add(matrix[rowEnd][i]);
+            if(rowStart <= rowEnd){
+                for(int i = colEnd ; i >= colStart ; i--){
+                    l.add(matrix[rowEnd][i]);
                 }
-
-                rowEnd--;
+              rowEnd--;
             }
-            if (colStart <= colEnd) {
 
-                for (int i = rowEnd; i >= rowStart; i--) {
+            if(colStart <= colEnd){
+                for(int i = rowEnd ; i >= rowStart ; i--){
+                    l.add(matrix[i][colStart]);
 
-                    list.add(matrix[i][colStart]);
                 }
-
                 colStart++;
             }
+
         }
 
-        return list;
+        return l;
 
+        
     }
 }
