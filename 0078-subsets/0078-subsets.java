687a309cf9109bd05a0c130d66cf9a850509258f
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-09-19 09:54:28 -0400

    Time: 1 ms (48.42%), Space: 43.2 MB (10.12%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,43 +1,21 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        return gen(nums.length-1 , new ArrayList<Integer>()  , nums);
-
+         List<List<Integer>> list = new ArrayList<>();
+         find(0,nums,list , new ArrayList<>());
+         return list;
+        
+        
     }
 
+    public void find(int idx , int[] nums , List<List<Integer>> list ,List<Integer> ds){
+         if(idx == nums.length){
+            list.add(new ArrayList<>(ds));
+            return;
+         }
 
-    public List<List<Integer>> gen(int index , List<Integer> ds  , int[] nums){
-
-        List<List<Integer>> ans = new ArrayList<>();
-        if(index == 0){
-
-           
-
-                ds.add(nums[0]);
-                ans.add(new ArrayList<>(ds));
-
-                ds.remove(ds.size()-1);
-
-                ans.add(new ArrayList<>(ds));
-
-                return ans;
-        }
-
-
-
-        ds.add(nums[index]);
-         
-         
-
-        List<List<Integer>> list1 = gen(index-1,ds,nums);
-
-        ds.remove(ds.size()-1);
-
-        List<List<Integer>> list2 = gen(index-1,ds,nums);
-
-        ans.addAll(list1);
-        ans.addAll(list2);
-        return ans;
-
-
+          ds.add(nums[idx]);
+          find(idx+1,nums,list,ds);
+          ds.remove(ds.size()-1);
+          find(idx+1,nums,list,ds);
     }
 }
