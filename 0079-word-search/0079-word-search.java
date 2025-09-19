cf2d464226c32ac22fcf9c193dc025965b9ebcc5
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-09-19 10:04:16 -0400

    Time: 145 ms (51.56%), Space: 41.4 MB (89.52%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,47 +1,45 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-
-        for(int i =0; i< board.length ; i++){
-            for(int j =0; j < board[0].length ; j++){
+        for(int i =0; i < board.length ; i++){
+            for(int j=0; j < board[0].length ; j++ ){
                 if(board[i][j] == word.charAt(0)){
-                     if(find(i,j,0,word,board)){
+                    boolean ans= find(i,j,board,word,0);
+                    if(ans){
                         return true;
-                     }
+                    }
                 }
             }
         }
-
         return false;
-
-
-        
     }
 
-    public boolean find(int row , int col , int idx , String word , char[][] board){
-
+    public boolean find(int r , int c , char[][] board , String word ,int idx){
         if(idx == word.length()){
-            return true ;
+            return true;
         }
-
-        if(row < 0 || col < 0 || row == board.length || col ==board[0].length){
+           if(r < 0 || c <0 || r == board.length || c== board[0].length){
             return false;
-        }
+           }
 
-        if(board[row][col] == word.charAt(idx)){
 
-            board[row][col] ='.';
 
-            boolean up = find(row+1,col,idx+1,word,board);
-            boolean down = find(row-1,col,idx+1,word,board);
-            boolean right = find(row,col+1,idx+1,word,board);
-            boolean left = find(row,col-1,idx+1,word,board);
+           if(word.charAt(idx) != board[r][c]){
+             return false;
+           }
 
-            board[row][col] =word.charAt(idx);
+           board[r][c] ='.';
 
-            return (up || down || right || left);
 
-        }
+          boolean one =  find(r+1,c,board,word,idx+1);
+          boolean two= find(r-1,c,board,word,idx+1);
+          boolean th= find(r,c+1,board,word,idx+1);
+          boolean four= find(r,c-1,board,word,idx+1);
 
-        return false;
+          board[r][c] = word.charAt(idx);
+
+          return one || two || th || four ;
+
+
+
     }
 }
