e4621669012d12624b3c038ff3ea0613d5a9121a
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-09-19 06:54:07 -0400

    Time: 1 ms (99.91%), Space: 46.4 MB (8.09%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean canJump(int[] nums) {
-       int nearidx =nums.length-1;
-      for(int i = nums.length-2; i >= 1 ;i--){
-           int maxJumps = nums[i];
-           if(i+maxJumps >= nearidx){
-            nearidx = i;
-           }
-       }
 
-       if(nums[0] >= nearidx){
-        return true;
-       }
-       else{
-        return false;
-       }
+        int n = nums.length;
 
+        int destidx = n-1;
+        for(int i =n-2; i >= 1; i-- ){
+            if(nums[i]+i >= destidx){
+                destidx =i;
+            }
+        }
+
+
+        return (nums[0] >= destidx);
+
+        
+        
     }
 }
