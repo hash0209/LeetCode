e5919e3da6a338ae960d329ba5753083d5524bf4
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-09-20 09:55:49 -0400

    Time: 1 ms (77.38%), Space: 45.5 MB (94.86%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -9,28 +9,5 @@ class Solution {
         return profit;
     }
 
-    public int find(int[] prices, int buy, int idx, Integer[][] dp) {
-
-        if (idx == prices.length) {
-            return 0;
-        }
-        if (dp[idx][buy] != null) {
-            return dp[idx][buy];
-        }
-
-        if (buy == 1) {
-            int buys = find(prices, 0, idx + 1, dp) - prices[idx];
-
-            int notbuy = find(prices, 1, idx + 1, dp);
-
-            return dp[idx][buy] = Math.max(buys, notbuy);
-        } else {
-            int sell = find(prices, 1, idx + 1, dp) + prices[idx];
-
-            int notsell = find(prices, 0, idx + 1, dp);
-
-            return dp[idx][buy] = Math.max(sell, notsell);
-        }
-
-    }
+    
 }
