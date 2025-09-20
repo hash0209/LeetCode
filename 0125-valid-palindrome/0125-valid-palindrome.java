8712520d38e85fecced6a128ec7b73f2b49d1824
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-09-20 10:29:08 -0400

    Time: 2 ms (98.81%), Space: 43.3 MB (60.11%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,31 +3,23 @@ class Solution {
         int l =0; int r = s.length()-1;
 
         while(l < r){
-               boolean lIsDigit = Character.isDigit(s.charAt(l));
-               boolean lIsLetter = Character.isLetter(s.charAt(l));
+              char left =s.charAt(l);
+              char right = s.charAt(r);
 
-               boolean rIsDigit = Character.isDigit(s.charAt(r));
-               boolean rIsLetter = Character.isLetter(s.charAt(r));
-
-
-               if((lIsDigit && rIsDigit) && (s.charAt(l)==s.charAt(r))){
-                   l++;
-                   r--;
-               }
-               else if((lIsLetter && rIsLetter ) && ((Character.toLowerCase(s.charAt(l))==(Character.toLowerCase(s.charAt(r)))))){
+              if(!Character.isLetterOrDigit(left)){
+                l++;
+              }
+              else if(!Character.isLetterOrDigit(right)){
+               r--;
+              }
+           else if(Character.toLowerCase(left)== Character.toLowerCase(right)){
                 l++;
                 r--;
-               }
-               
-               else if(!lIsDigit && !lIsLetter){
-                     l++;
-               }
-               else if(!rIsDigit && !rIsLetter){
-                r--;
-               }
-               else{
+              }
+              else{
                 return false;
-               }
+              }
+
 
         }
         return true;
