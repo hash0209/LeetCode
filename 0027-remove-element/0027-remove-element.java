33750ff1eaf927cc13b68b5a98ec697665b77b41
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-09-21 11:38:59 -0400

    Time: 0 ms (100%), Space: 42.2 MB (17.41%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,16 +1,16 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-
-
-        int write = 0;
+      
+        int write =0;
 
         for(int read =0; read < nums.length ; read++){
-            if(nums[read]!=val){
-                nums[write ]= nums[read];
+            if(nums[read] != val){
+                nums[write] = nums[read];                
                 write++;
             }
         }
         return write;
+
         
     }
 }
