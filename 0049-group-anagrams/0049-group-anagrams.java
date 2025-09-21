9e723a4004d20a45cc617399d6219130a25b38ba
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-09-21 14:10:27 -0400

    Time: 34 ms (7.37%), Space: 49.6 MB (5.55%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,48 +1,39 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
 
-        Map<String,List<String>> map = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
 
-        
-
-        for(int i =0 ; i < strs.length ; i++){
-            
-            
-            
+        for (int i = 0; i < strs.length; i++) {
 
             int[] freq = new int[26];
-            StringBuilder key = new StringBuilder();
-
-            for(char c : strs[i].toCharArray() ){
-                freq[c-'a']++;
-                   
-                  
-            }
-
-            for(int  j = 0; j < freq.length ; j++){
-                key.append(freq[j] + "#");
-                
-            }
-
-            String k = key.toString();
-
-            map.putIfAbsent(k,new ArrayList<>());
-            map.get(k).add(strs[i]);
            
-            
-             
+
+            for (char c : strs[i].toCharArray()) {
+                freq[c - 'a']++;
+
+            }
+
+             StringBuilder keyBuilder = new StringBuilder();
+
+            for (int j = 0; j < freq.length; j++) {
+                keyBuilder.append(freq[j] + "#");
+
+            }
+
+            String key = keyBuilder.toString();
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(strs[i]);
+
         }
-
-
 
         List<List<String>> res = new ArrayList<>();
 
-        for(List<String> l : map.values()){
-               res.add(l);
+        for (List<String> l : map.values()) {
+            res.add(l);
         }
 
         return res;
 
-        
     }
 }
