8751174d5f07b98e3cd5be274621c11ac16533a1
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-09-22 11:55:36 -0400

    Time: 0 ms (100%), Space: 44.4 MB (72.93%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,45 +15,46 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
+        
         if(root == null){
             return true;
         }
-        int left = find(root.left);
-        int right = find(root.right);
+        int l = find(root.left);
+        int r = find(root.right);
 
-        if(left == -1 || right == -1){
+
+        if(l ==-1 || r== -1){
             return false;
         }
 
-        int diff = Math.abs(left-right);
-        if(diff <= 1){
-            return true;
+        int diff = Math.abs(l-r);
+        if(diff >1){
+            return false;
         }
-        return false;
-
-    
-
+        return true;
+        
+        
+        
     }
 
-
-    public int find(TreeNode root  ){
-        if(root == null){
-           return 0;
+    public int find(TreeNode n){
+        if(n == null){
+            return 0;
         }
 
-        int l =  find(root.left);
-        int r =  find(root.right);
+        int l = find(n.left);
+        int r = find(n.right);
 
-        if( l == -1 || r ==-1){
+
+        if(l ==-1 || r== -1){
             return -1;
         }
 
         int diff = Math.abs(l-r);
-        if(diff <= 1){
-            return 1+Math.max(l,r);
+        if(diff >1){
+            return -1;
         }
-        return -1;
 
-        
+        return 1 + Math.max(l,r) ;
     }
 }
