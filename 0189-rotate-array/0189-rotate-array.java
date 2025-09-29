e9344082ef2e30990b31e34deea0fa3e9a80d4eb
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-09-29 16:04:34 -0400

    Time: 0 ms (100%), Space: 57.3 MB (79.39%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -6,14 +6,12 @@ class Solution {
         
 
         k=k%n;
-     //   if(k == 0){
-      //      return ; 
-      //  }
+    
 
-        k = n-k-1;
+        k = n-k;
 
-        reverse(0,k,nums);
-        reverse(k+1,n-1,nums);
+        reverse(0,k-1,nums);
+        reverse(k,n-1,nums);
         reverse(0,n-1,nums);
 
 
