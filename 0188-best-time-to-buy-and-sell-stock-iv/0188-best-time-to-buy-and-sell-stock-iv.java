47dc9e433658042240fb4ee32b9077a02abd2eef
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-09-29 15:01:56 -0400

    Time: 15 ms (7.4%), Space: 54.3 MB (5.36%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,41 +1,40 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
 
-        int[][][] dp = new int[prices.length][2][k+1];
-
-        for(int t = k; t > 0 ; t--){
-            dp[prices.length-1][0][t] = prices[prices.length-1];
-        }
-
-
-        for(int idx = prices.length-2 ; idx >= 0 ; idx--){
-
-            for(int buy =0; buy <= 1; buy ++){
-
-
-                for(int t =1 ; t <= k ; t++){
-
-                    if(buy ==1){
-
-                        int buys = dp[idx+1][0][t] - prices[idx];
-
-                        int notbuys = dp[idx+1][buy][t];
-
-                        dp[idx][buy][t] =Math.max(buys,notbuys);
-
-                    
-                    }
-                    else{
-                        int sell = dp[idx+1][1][t-1] + prices[idx];
-                        int notsell = dp[idx+1][buy][t];
-
-                        dp[idx][buy][t] = Math.max(sell,notsell);
-                    }
-                }
-            }
-        }
-
-        return dp[0][1][k];
+            Integer[][][] dp = new Integer[prices.length][k+1][2];
+              
+             return find(0,prices,k,1,dp);
         
+    }
+
+    public int find(int idx , int[] prices , int k , int buy , Integer[][][] dp){
+
+        if(k == 0){
+            return 0;
+        }
+        if(idx == prices.length){
+            return  0;
+        }
+
+        if(dp[idx][k][buy]!=null){
+            return dp[idx][k][buy];
+        }
+
+        if(buy == 1){
+            int buys = find(idx+1,prices , k , 0,dp) -prices[idx];
+
+            int notbuys = find(idx+1 , prices , k , 1,dp );
+
+            return dp[idx][k][buy] = Math.max(buys,notbuys);    
+        }
+        else{
+
+            int sell = find(idx+1 , prices , k-1 , 1,dp)+prices[idx];
+
+            int notsell = find(idx+1,prices, k , 0,dp) ;
+
+            return dp[idx][k][buy] =Math.max(sell,notsell);
+
+        }
     }
 }
