18c0f389eb7737630355202a3835f9e4c9b63a78
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-09-30 09:04:38 -0400

    Time: 0 ms (100%), Space: 41.1 MB (61.89%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,19 +1,20 @@
 class Solution {
     public int rob(int[] nums) {
-      
-        int prev = nums[0] ; int prev2 = 0;
 
-        for(int idx =1 ; idx < nums.length ; idx++){
+       
+        int prev2 =0 ; int prev = nums[0];
+       
 
-               int pick = prev2 + nums[idx] ;
-
-               int notpick =prev;
-
-               prev2 = prev ;
-               prev = Math.max(pick,notpick);
-
+        for(int idx =1; idx < nums.length ; idx++){
+            int rob = prev2+nums[idx];
+            int notrob = prev;
+            prev2=prev;
+            prev=Math.max(rob,notrob);
         }
 
         return prev;
+        
     }
+
+
 }
