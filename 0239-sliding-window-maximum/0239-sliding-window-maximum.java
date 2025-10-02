0102367046b2eae27cae65c93d62617eb069dfc5
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-10-02 09:33:41 -0400

    Time: 38 ms (24.69%), Space: 61.1 MB (29.94%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,53 +1,32 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
 
+        ArrayList<Integer> list = new ArrayList<>();
 
+        Deque<Integer> dq = new ArrayDeque<>();
 
-        ArrayList<Integer> ans = new ArrayList<>();
-        
+        for (int i = 0; i < nums.length; i++) {
+            while (!dq.isEmpty() && dq.peekFirst() < i - k + 1) {
+                dq.pollFirst();
+            }
 
+            while (!dq.isEmpty() && nums[i] > nums[dq.peekLast()]) {
+                dq.pollLast();
+            }
 
-        ArrayDeque<Integer> q = new ArrayDeque<>();
-        q.push(0);
+            dq.offerLast(i);
 
-
-       for(int i =1; i < k ; i++){
-        while(!q.isEmpty() && nums[q.peek()]< nums[i]){
-                    q.pop();
-        }
-        q.push(i);
-       }
-      
-       ans.add(nums[q.peekLast()]);
-
-        for(int i =k; i < nums.length ; i++ ){
-             while(!q.isEmpty() && nums[q.peek()]< nums[i]){
-                    q.pop();
-        }
-        q.push(i);
-
-        if(q.peekLast() > i-k){
-            ans.add(nums[q.peekLast()]);
-        }
-        else{
-            q.pollLast();
-          // System.out.println(el);
-            ans.add(nums[q.peekLast()]);
+            if (i >= k - 1) {
+                list.add(nums[dq.peekFirst()]);
+            }
         }
 
-
-
+        int[] res = new int[list.size()];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = list.get(i);
         }
 
+        return res;
 
-        int[] arr = new int[ans.size()];
-        int idx =0;
-
-        for(Integer x : ans){
-            arr[idx] = x;
-            idx ++;
-        }
-
-        return arr;
     }
 }
