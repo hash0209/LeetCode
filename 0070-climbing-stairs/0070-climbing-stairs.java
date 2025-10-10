6938cdbd76bc9f0324b85f638fe3205205f67f3f
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-10-10 09:48:08 -0400

    Time: 0 ms (100%), Space: 40.3 MB (83.02%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,24 +1,29 @@
 class Solution {
     public int climbStairs(int n) {
 
-        int prev = 1; 
-        int prev2= 1;
-
-        for(int i = 2 ; i <= n ; i++){
-            int curr = prev+prev2;
-            prev2 = prev;
-            prev = curr;
-
-        }
-        return prev;
-
+     
 
         
+
+        int prev =1 ; int prev2 =0;
+
+        for(int i =1 ; i <= n ; i++){
+            int one = prev;
+            int two = prev2;
+
+            prev2 = prev;
+
+            
+
+            prev = one+two;
+        }
+
+        return prev;
 
        
         
     }
 
 
-
+    
 }
