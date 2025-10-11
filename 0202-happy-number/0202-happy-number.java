3b477744353c17ad44da3ffc904b848809bb867b
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-10-11 11:40:08 -0400

    Time: 0 ms (100%), Space: 40.6 MB (75.81%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,34 +1,40 @@
 class Solution {
     public boolean isHappy(int n) {
         
-        Set<Integer> set = new HashSet<>();
+        if(n ==1){
+            return true;
+        }
 
+        int slow = n;
+        int fast = n;
 
-      
+        while(true){
+            
+             
+             slow = sumOfSquares(slow);
+             fast = sumOfSquares(sumOfSquares(fast));
+             if(slow ==1){
+                return true;
+             }
+             if(slow == fast){
+               break;
+             }
 
-       while(n!=1){
-        set.add(n);
-    
-        int sum =0 ;
+        }
+        return false;
+    }
 
-        while(n !=0){
-           int digit = n%10;
+    public int sumOfSquares(int n ){
+        int sum =0;
+
+        while(n!=0){
+            int digit = n%10;
             sum+= Math.pow(digit,2);
             n=n/10;
+          
         }
-
-        if(set.contains(sum)){
-            return false;
-        }
-
-        n = sum;
-       
-
-
-
-
-       }
-       return true;
-
+        return sum;
+          
+    
     }
 }
