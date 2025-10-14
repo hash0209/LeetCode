419f0627e3e137bd9b40425d0a2a41a83970cd9d
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-10-14 12:22:32 -0400

    Time: 1 ms (52.54%), Space: 41.7 MB (9.38%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -6,17 +6,12 @@ class Solution {
             return false;
         }
 
-        while(n!=1 && ((n%2 ==0 || n%3 == 0 || n%5 == 0))){
-            if(n%2 == 0){
-                n =n/2;
-            }
-            else if(n%3 == 0){
-                n= n/3;
-            }
-            else if(n%5 == 0){
-                n = n/5;
-            }
-            
+        int[] nums = new int[]{2,3,5};
+
+        for(int i =0; i < nums.length ; i++){
+               while(n%nums[i] == 0){
+                  n = n/nums[i];
+               }
         }
 
         return (n==1) ? true : false;
