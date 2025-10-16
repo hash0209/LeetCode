a9ac7ae9dea4167df02ef97bb5ff1f38a18f75b1
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-10-16 12:28:58 -0400

    Time: 2 ms (96.61%), Space: 43.2 MB (75.26%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
 
         if(nums1.length > nums2.length){
-            intersection(nums2,nums1);
+            return intersection(nums2,nums1);
         }
 
         Set<Integer> list = new HashSet<>();
