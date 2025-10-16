50594594c1a60d9a707266e49016899de1d76192
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-10-16 12:27:10 -0400

    Time: 2 ms (96.61%), Space: 43.7 MB (14.3%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+
+        if(nums1.length > nums2.length){
+            intersection(nums2,nums1);
+        }
+
+        Set<Integer> list = new HashSet<>();
+
+        Set<Integer> set = new HashSet<>();
+
+        for(Integer i : nums2){
+            set.add(i);
+        }
+
+        for(Integer i : nums1){
+            if(set.contains(i)){
+                list.add(i);
+            }
+        }
+
+
+        int[] res = new int[list.size()];
+        int idx =0;
+
+        for(Integer i : list){
+              res[idx] = i;
+              idx++;
+        }
+
+        return res;
+        
+    }
+}
