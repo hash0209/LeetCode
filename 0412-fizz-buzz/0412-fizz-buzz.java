28852085587be0754e5e9cb809a9c137b98bc91c
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-10-18 11:11:36 -0400

    Time: 1 ms (99.65%), Space: 45.2 MB (60.03%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -3,47 +3,24 @@ class Solution {
 
         List<String> list = new ArrayList<>();
 
-        for(int i =0; i < n ; i++){
-            list.add("");
+       
+        for (int i = 0; i < n; i++) {
+            int num = i + 1;
 
-        }
-        for(int i =0; i < n ; i++){
-            int num =i+1;
+            if (num % 3 == 0 && num % 5 == 0) {
+                list.add( "FizzBuzz");
 
-            if(num % 3== 0 && num%5 == 0){
-                    list.set(i,"FizzBuzz");
-
-                }
-                else if(num%3 == 0){
-                     list.set(i,"Fizz");
-             }
-             else if(num%5==0){
-                  list.set(i,"Buzz");
-             }
-             else{
-                 list.set(i,String.valueOf(num));
-             }
-
-
-        //}
-
-       /* for(int i =0; i < n ; i++){
-            if(!list.get(i).isEmpty()){
-                continue;
+            } else if (num % 3 == 0) {
+                list.add( "Fizz");
+            } else if (num % 5 == 0) {
+                list.add( "Buzz");
+            } else {
+                list.add( String.valueOf(num));
             }
-             int num =i+1;
-             int factor =1;
 
-             while(num <= n){
-                
-            factor++;
-            num=num*factor;
-             
-             
+          
+        }
+        return list;
 
-             
-
-        } */
-        }return list;
-
-}}
+    }
+}
