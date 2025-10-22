bae8e95edc460616ec55fd7da418d1aba9dd2cf5
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-10-22 13:56:53 -0400

    Time: 0 ms (100%), Space: 40.4 MB (71.47%) - LeetHub

--- 0796-reaching-points/0796-reaching-points.java
+++ 0796-reaching-points/0796-reaching-points.java
@@ -1,17 +1,23 @@
 class Solution {
- public static boolean reachingPoints(int sx, int sy, int tx, int ty) {
+    public static boolean reachingPoints(int sx, int sy, int tx, int ty) {
 
-        while (sx < tx && sy < ty)
-            if (tx < ty)
+        while (sx < tx && sy < ty) {
+            if (tx < ty){
                 ty %= tx;
-            else
+            }
+            else{
                 tx %= ty;
+            }
+        }
 
-      if(sx == tx && sy <= ty && (ty - sy) % sx == 0)
-            return true; 
-        
-         return sy == ty && sx <= tx && (tx - sx) % sy == 0;
+        if (sx == tx && sy == ty) {
+            return true;
+        }
+
+        if (sx == tx && ty > sy && (ty - sy) % tx == 0)
+            return true;
+
+        return (sy == ty && tx > sx && (tx - sx) % ty == 0);
     }
-
 
 }
