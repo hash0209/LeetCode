1f976da42a777029ce8c7a26518688a328e00666
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-11-20 11:44:19 -0500

    Time: 2179 ms (5%), Space: 74.8 MB (5.56%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,16 +1,44 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
 
-     PriorityQueue<Integer> maxHeap =new PriorityQueue<>();
-        for(Integer i: nums){
-            maxHeap.add(i);
-            if(maxHeap.size()> k){
-                maxHeap.poll();
-            }
-        }
+                return sort(0,nums.length-1,nums,k-1);
 
-        return maxHeap.poll();
         
-        
+    }
+
+
+
+    public int sort(int start , int end , int[] nums , int k ){
+        if(start==end){
+            return nums[start];
+        }
+           int pIndex = start ;
+           int pivot = nums[end];
+
+
+           for(int i = start;  i < end ; i++){
+                    if(nums[i] > pivot){
+                        swap(i , pIndex,nums);
+                        pIndex++;
+                    }
+           }
+           swap(pIndex,end,nums);
+
+           if(pIndex == k){
+            return nums[pIndex];
+           }
+
+           if(pIndex > k){
+            return sort(start , pIndex-1,nums,k);
+           }
+           else{
+            return sort(pIndex+1,end,nums,k);
+           }
+    }
+
+    public void swap(int left , int right , int[] nums){
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] =temp;
     }
 }
