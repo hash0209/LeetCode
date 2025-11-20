f388447348c08af40ec0a67e102d62749b20f8e8
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-11-20 08:50:58 -0500

    Time: 17 ms (16.06%), Space: 51.5 MB (5.34%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,44 +1,51 @@
 class Solution {
+
+    // 1. Create a frequency hashMap
+    //2 . add nums with frequency to Map
+    //3 . Create an arrayList of ArrayList
+    //4. initialize with n empty ArrayList (bucket)
+    //5. iterate over the keys in the map and add it to the respective frequency bucket
+    //6. iterate over buckets in reverse and get k elements
     public int[] topKFrequent(int[] nums, int k) {
 
-        Map<Integer,Integer> freq = new HashMap<>(); 
-
+        HashMap<Integer,Integer> freqMap = new HashMap<>();
         for(Integer i : nums){
-
-               freq.putIfAbsent(i , 0);
-               freq.put(i , freq.get(i)+1);
+              int freq = freqMap.getOrDefault(i,0);
+            
+              freqMap.put(i,freq+1);
         }
 
 
-        List<List<Integer>> list = new ArrayList<>();
+        List<List<Integer>> buckets = new ArrayList<>();
         for(int i =0; i <= nums.length ; i++){
-            list.add(new ArrayList<>());
+            buckets.add(new ArrayList<>());
         }
 
-        
-        for(Integer key : freq.keySet()){
-
-            int f = freq.get(key);
-            list.get(f).add(key);
-
+        for(Integer key : freqMap.keySet()){
+              buckets.get(freqMap.get(key)).add(key);
         }
-
-
 
         int[] res = new int[k];
-
         int idx = k-1;
-        for(int i = list.size()-1 ; i >= 0 ; i--){
-            List<Integer> l = list.get(i);
-             for(int j = 0;  j < l.size() ; j++){
-                if(idx < 0){
-                    break;
-                }
-                     res[idx] = l.get(j);
-                     idx--;
-             }
+
+
+        for(int i =  buckets.size()-1; i >= 0 ; i--){
+
+                 List<Integer> bucket = buckets.get(i);
+
+                 for(Integer num : bucket){
+                    if(idx < 0){
+                        break;
+                    }
+                    res[idx] = num;
+                    idx--;
+                 }
+
+
         }
         return res;
+        
+
         
     }
 }
