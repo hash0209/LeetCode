801e6543e8b3973c4ef30341b0ce1d6895b57bb7
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2025-11-20 11:18:41 -0500

    Time: 59 ms (58.07%), Space: 67.8 MB (24.72%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,32 +1,19 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        return quickSelect(nums, 0, nums.length - 1, k - 1); // 0-indexed
-    }
 
-    private int quickSelect(int[] nums, int left, int right, int k) {
-        if (left == right) return nums[left]; // only one element
-
-        int pivotIndex = right;
-        int pivot = nums[pivotIndex];
-        //swap(pivotIndex, right, nums); // move pivot to end
-
-        int pIndex = left;
-        for (int i = left; i < right; i++) {
-            if (nums[i] > pivot) { // larger elements to left
-                swap(i, pIndex, nums);
-                pIndex++;
-            }
+        PriorityQueue<Integer> maxHeap =new PriorityQueue<>((a,b) -> Integer.compare(b,a));
+        for(Integer i: nums){
+            maxHeap.add(i);
         }
-        swap(pIndex, right, nums); // pivot in correct place
 
-        if (pIndex == k) return nums[pIndex];
-        else if (pIndex > k) return quickSelect(nums, left, pIndex - 1, k);
-        else return quickSelect(nums, pIndex + 1, right, k);
-    }
+        int ans = 0;
 
-    private void swap(int i, int j, int[] nums) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
+        while(k!=1){
+            maxHeap.poll();
+            k--;
+        }
+        return maxHeap.poll();
+        
+        
     }
 }
