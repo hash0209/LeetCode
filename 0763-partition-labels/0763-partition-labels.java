0b5e5b109913dd8fc898ae2dfcc909aa099873ee
Harshita Jain <48876801+hash0209@users.noreply.github.com>
2026-01-06 07:15:05 -0500

    Time: 9 ms (39.38%), Space: 42.5 MB (99.97%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        
+        List<Integer>  ans = new ArrayList<>();
+        Map<Character , Integer> map = new HashMap<>();
+
+        for(int i =0 ; i < s.length() ; i++){
+            map.put(s.charAt(i),i);
+        }
+        int start =0; 
+        int end = 0;
+
+        for(int i =0 ; i < s.length();i++){
+            end = Math.max(end,map.get(s.charAt(i)));
+
+            if(i == end ){
+                ans.add(end-start+1);
+                start = end+1;
+            }
+            
+        }
+        return ans;
+    }
+}
